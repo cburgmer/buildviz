d27305a151f3fbcd9b903ee855ffafa21b1e715f
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-05-10 19:12:14 +0800

    Randomize build entries

--- seedDummyData.sh
+++ seedDummyData.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-function build {
-    START=$1
-    END=$2
+function aBuild {
+    START=$RANDOM
+    END=$[ $START + ( $RANDOM % 100 ) ]
     echo '{"start":' $START ', "end":' $END '}'
 }
 
@@ -12,11 +12,11 @@ function send {
     curl -H "Content-Type: application/json" --data @- -XPUT "http://localhost:3000/builds/${JOB}/${BUILD}"
 }
 
-build 0 3 | send "someBuild" 1
-build 10 30 | send "someBuild" 2
-build 35 40 | send "someBuild" 3
+aBuild | send "someBuild" 1
+aBuild | send "someBuild" 2
+aBuild | send "someBuild" 3
 
-build 100 200 | send "anotherBuild" 1
+aBuild | send "anotherBuild" 1
 
-build 0 10 | send "yetAnotherBuild" 1
-build 20 30 | send "yetAnotherBuild" 2
+aBuild | send "yetAnotherBuild" 1
+aBuild | send "yetAnotherBuild" 2
