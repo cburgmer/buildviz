3a5b96b78313ebb4af9a3f8c133bedabcaf1b8f0
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-06-15 22:05:37 +0800

    Show text with failures

--- resources/public/js/failures.js
+++ resources/public/js/failures.js
@@ -1,21 +1,26 @@
 (function (widget) {
-    // Following http://bl.ocks.org/mbostock/4063423
+    // Following http://bl.ocks.org/metmajer/5480307
     var diameter = 600,
         className = "failures";
 
     var radius = diameter / 2,
         color = d3.scale.category20c();
 
+    var x = d3.scale.linear()
+            .range([0, 2 * Math.PI]);
+
+    var y = d3.scale.linear()
+            .range([0, radius]);
+
     var partition = d3.layout.partition()
             .sort(null)
-            .size([2 * Math.PI, radius * radius])
-            .value(function(d) { return 1; });
+            .value(function(d) { return d.size; });
 
     var arc = d3.svg.arc()
-            .startAngle(function(d) { return d.x; })
-            .endAngle(function(d) { return d.x + d.dx; })
-            .innerRadius(function(d) { return Math.sqrt(d.y); })
-            .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+            .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+            .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+            .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+            .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
     var svg = widget.create("Failures", "Color: Job/Test Suite, Arc size: Number of Failures")
             .svg(diameter)
@@ -73,22 +78,36 @@
         return color(key);
     };
 
+    var computeTextRotation = function (d) {
+        return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+    };
+
     d3.json('/failures', function (_, failures) {
         var data = {
             name: "Failures",
             children: transformFailures(failures)
         };
 
-        var path = svg.datum(data).selectAll("path")
-                .data(partition.value(function (d) { return d.size; }).nodes)
+        var g = svg.datum(data).selectAll("g")
+                .data(partition.nodes)
                 .enter()
-                .append("path")
-                .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-                .attr("d", arc)
-                .style("stroke", "#fff")
-                .style("fill", inheritDirectParentColorForLeafs);
+                .append("g");
 
-        path.append("title")
+        g.append("path")
+            .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+            .attr("d", arc)
+            .style("stroke", "#fff")
+            .style("fill", inheritDirectParentColorForLeafs);
+
+        g.append("text")
+            .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+            .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+            .attr("x", function(d) { return y(d.y); })
+            .attr("dx", "6") // margin
+            .attr("dy", ".35em") // vertical-align
+            .text(function(d) { return d.name; });
+
+        g.append("title")
             .text(function (d) {
                 var failures = d.size ? ' (' + d.size + ')' : '';
                 return d.name + failures;
