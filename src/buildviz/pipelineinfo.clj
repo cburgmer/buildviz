7438848738a2d8760be0fe218fb500c3fa845a6d
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-07-02 09:04:09 +0800

    Refactor

--- src/buildviz/pipelineinfo.clj
+++ src/buildviz/pipelineinfo.clj
@@ -8,33 +8,41 @@
    :culprits #{culprit}
    :ongoing-culprits #{culprit}})
 
+(defn- open-new-fail-phase [fail-phases {job :job end :end}]
+  (conj fail-phases (new-ongoing-phase end job)))
+
+(defn- add-failing-build-to-fail-phase [fail-phases ongoing-fail-phase {job :job}]
+  (let [culprits (conj (:culprits ongoing-fail-phase) job)
+        ongoing-culprits (conj (:ongoing-culprits ongoing-fail-phase) job)]
+    (update-ongoing-fail-phase fail-phases
+                               (assoc ongoing-fail-phase
+                                      :culprits culprits
+                                      :ongoing-culprits ongoing-culprits))))
+
+(defn- pop-passing-build-from-fail-phase [fail-phases ongoing-fail-phase {job :job end :end}]
+  (let [ongoing-culprits (disj (:ongoing-culprits ongoing-fail-phase) job)]
+    (update-ongoing-fail-phase fail-phases
+                               (assoc ongoing-fail-phase
+                                      :ongoing-culprits ongoing-culprits
+                                      :end end))))
+
 (defn- accumulate-fail-phases [fail-phases build]
   (let [outcome (:outcome build)
-        end (:end build)
         job (:job build)]
     (if (or (empty? fail-phases)
-             (empty? (:ongoing-culprits (last fail-phases))))
+            (empty? (:ongoing-culprits (last fail-phases))))
 
-      (if (= "fail" outcome)
-        (conj fail-phases (new-ongoing-phase end job))
+      (if (= outcome "fail")
+        (open-new-fail-phase fail-phases build)
         fail-phases)
 
       (let [ongoing-fail-phase (last fail-phases)]
-        (if (and (= "pass" outcome)
+        (if (and (= outcome "pass")
                  (contains? (:ongoing-culprits ongoing-fail-phase) job))
-          (let [ongoing-culprits (disj (:ongoing-culprits ongoing-fail-phase) job)]
-            (update-ongoing-fail-phase fail-phases
-                                  (assoc ongoing-fail-phase
-                                         :ongoing-culprits ongoing-culprits
-                                         :end end)))
+          (pop-passing-build-from-fail-phase fail-phases ongoing-fail-phase build)
 
-          (if (= "fail" outcome)
-            (let [culprits (conj (:culprits ongoing-fail-phase) job)
-                  ongoing-culprits (conj (:ongoing-culprits ongoing-fail-phase) job)]
-              (update-ongoing-fail-phase fail-phases
-                                    (assoc ongoing-fail-phase
-                                           :culprits culprits
-                                           :ongoing-culprits ongoing-culprits)))
+          (if (= outcome "fail")
+            (add-failing-build-to-fail-phase fail-phases ongoing-fail-phase build)
             fail-phases))))))
 
 (defn pipeline-fail-phases [builds]
