306baf544749227a2febaaa5dd75910f06c54081
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-10-02 23:00:54 +0800

    Replace bubbles with blocks for average job runtime

--- resources/public/widgets/averageJobRuntime.js
+++ resources/public/widgets/averageJobRuntime.js
@@ -2,7 +2,7 @@
     // Roughly following http://bl.ocks.org/mbostock/4063269
     var diameter = 600;
 
-    var buildRuntimeAsBubbles = function (pipeline) {
+    var buildEntries = function (pipeline) {
         return Object.keys(pipeline)
             .filter(function (jobName) {
                 return pipeline[jobName].averageRuntime;
@@ -19,18 +19,30 @@
             });
     };
 
+    var buildHierarchy = function (jobEntries) {
+        var hierarchy = d3.nest()
+                .key(function (d) {
+                    return d.name.split(' ')[0];
+                })
+                .entries(jobEntries);
+        return hierarchy.map(function (group) {
+            return {
+                name: group.key,
+                children: group.values
+            };
+        });
+    };
+
     var svg = widget.create("Average job runtime",
                             "Color: jobGroup, Diameter: avg. Runtime",
                             "/jobs.csv")
             .svg(diameter);
 
-    var bubble = d3.layout.pack()
-            .sort(null)
+    var treemap = d3.layout.treemap()
             .size([diameter, diameter])
-            .padding(1.5),
-        noGrouping = function (bubbleNodes) {
-            return bubbleNodes.filter(function(d) { return !d.children; });
-        };
+            .sort(function (a, b) {
+                return a.value - b.value;
+            });
 
     dataSource.load('/jobs', function (root) {
         var jobNames = Object.keys(root);
@@ -41,25 +53,38 @@
 
         var color = jobColors.colors(jobNames);
 
-        var node = svg.selectAll("g")
-                .data(noGrouping(bubble.nodes({children: buildRuntimeAsBubbles(root)})))
+        var node = svg
+                .datum({children: buildHierarchy(buildEntries(root))})
+                .selectAll("g")
+                .data(treemap.nodes)
                 .enter()
                 .append("g")
-                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });;
+                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+        node.append('rect')
+            .attr('width', function (d) {
+                return d.dx;
+            })
+            .attr('height', function (d) {
+                return d.dy;
+            })
+            .style('fill', function (d) {
+                if (d.name) {
+                    return color(d.name);
+                }
+                return 'transparent';
+            });
 
         node.append("title")
             .text(function(d) { return d.title; });
 
-        node.append("circle")
-            .attr("r", function (d) { return d.r; })
-            .style("fill", function(d) {
-                return color(d.name);
-            });
-
         node.append("text")
+            .attr("transform", function(d) { return "translate(" + (d.dx / 2) + "," + (d.dy / 2) + ")"; })
             .style("text-anchor", "middle")
             .each(function (d) {
-                widget.textWithLineBreaks(this, d.name.split(' '));
+                if (!d.children && d.name && d.dx > 90 && d.dy > 50) {
+                    widget.textWithLineBreaks(this, d.name.split(' '));
+                }
             });
     });
 }(widget, utils, jobColors, dataSource));
