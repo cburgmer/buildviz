d04d3985f016876c0a064a550ce1d701e4724736
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-10-09 23:16:10 +0800

    Only show most failing tests, cut down noise

--- resources/public/widgets/testFailures.js
+++ resources/public/widgets/testFailures.js
@@ -1,9 +1,10 @@
 (function (widget, zoomableSunburst, dataSource) {
     // Following http://bl.ocks.org/metmajer/5480307
-    var diameter = 600;
+    var diameter = 600,
+        testCountPerJob = 5;
 
-    var svg = widget.create("Test failures",
-                            "Color: Job/Test Suite, Arc size: Number of Failures",
+    var svg = widget.create("Most frequently failing tests",
+                            "Color: job/test suite, arc size: number of test failures",
                            "/failures.csv")
             .svg(diameter);
 
@@ -14,20 +15,30 @@
         return entry.name + failures;
     };
 
-    var hasOnlyOneChildThatIsNotLeaf = function (node) {
-        return node.children && node.children.length === 1 && node.children[0].children !== undefined;
+    var hasOnlyOneChildThatIsNotLeaf = function (children) {
+        return children && children.length === 1 && children[0].children !== undefined;
     };
 
     var skipTestSuiteWithOnlyOneClassOrNestedSuite = function (testSuite) {
-        var testSuiteHasOnlyOneChild = hasOnlyOneChildThatIsNotLeaf(testSuite);
+        var testSuiteHasOnlyOneChild = hasOnlyOneChildThatIsNotLeaf(testSuite.children);
 
         return testSuiteHasOnlyOneChild ? testSuite.children[0] : testSuite;
     };
 
-    var skipOnlyTestSuite = function (job) {
-        var hasOnlyOneTestSuite = hasOnlyOneChildThatIsNotLeaf(job);
+    var skipOnlyTestSuite = function (children) {
+        var nonRemainderNodes = children.filter(function (c) {
+            return !c.remainder;
+        });
+        var hasOnlyOneTestSuite = hasOnlyOneChildThatIsNotLeaf(nonRemainderNodes);
 
-        return hasOnlyOneTestSuite ? job.children[0].children : job.children;
+        if (hasOnlyOneTestSuite) {
+            return children[0].children
+                .concat(children.filter(function (c) {
+                    return c.remainder;
+                }));
+        } else {
+            return children;
+        }
     };
 
     var transformNode = function (node) {
@@ -40,34 +51,74 @@
         if (elem.children) {
             e.children = elem.children.map(transformNode);
         } else {
-            e.size = elem.discountedFailedCount;
+            e.size = elem.failedCount;
             e.title = title(elem);
         }
         return e;
     };
 
-    var totalTestCaseFailures = function (node) {
-        if (node.children) {
-            return d3.sum(node.children.map(totalTestCaseFailures));
-        } else {
-            return node.failedCount;
-        }
+    var flattenTests = function (testsuites) {
+        return testsuites.reduce(function (tests, testsuite) {
+            var suiteTests = testsuite.children.reduce(function (tests, testClass) {
+                return tests.concat(testClass.children.map(function (test) {
+                    return {
+                        name: test.name,
+                        failedCount: test.failedCount,
+                        testSuite: testsuite.name,
+                        testClass: testClass.name
+                    };
+                }));
+            }, []);
+
+            return tests.concat(suiteTests);
+        }, []);
     };
 
-    var setDiscountedFailure = function (node, discountFactor) {
-        if (node.children) {
-            node.children.forEach(function (child) {
-                setDiscountedFailure(child, discountFactor);
+    var unflattenTests = function (tests) {
+        return d3.nest()
+            .key(function (d) {
+                return d.testSuite;
+            })
+            .key(function (d) {
+                return d.testClass;
+            })
+            .entries(tests)
+            .map(function (suite) {
+                return {
+                    name: suite.key,
+                    children: suite.values.map(function (testClass) {
+                        return {
+                            name: testClass.key,
+                            children: testClass.values
+                        };
+                    })
+                };
             });
-        } else {
-            node.discountedFailedCount = node.failedCount * discountFactor;
-        }
     };
 
-    var normalizeJobsByJobFailedCount = function (job) {
-        var normalizationFactor = job.failedCount / totalTestCaseFailures(job);
+    var filterMostNFailingTests = function (testsuites, n) {
+        var tests = flattenTests(testsuites);
 
-        setDiscountedFailure(job, normalizationFactor);
+        tests.sort(function (a, b) {
+            return a.failedCount - b.failedCount;
+        });
+
+        var slowestTests = tests.slice(-n),
+            remaindingTotalFailedCount = tests.slice(0, -n).reduce(function (totalFailedCount, test) {
+                return totalFailedCount + test.failedCount;
+            }, 0);
+
+        var suites = unflattenTests(slowestTests);
+
+        if (remaindingTotalFailedCount) {
+            suites.push({
+                name: 'Remaining average',
+                failedCount: remaindingTotalFailedCount / (tests.length - n),
+                remainder: true
+            });
+        }
+
+        return suites;
     };
 
     var transformFailures = function (failureMap) {
@@ -78,9 +129,7 @@
             })
             .map(function (jobName) {
                 var job = failureMap[jobName],
-                    children = skipOnlyTestSuite(job);
-
-                normalizeJobsByJobFailedCount(job);
+                    children = skipOnlyTestSuite(filterMostNFailingTests(job.children, testCountPerJob));
 
                 return {
                     name: jobName,
