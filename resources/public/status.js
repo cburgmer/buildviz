c1ca2eb3cd99ec8be22b93efb3bd94096305747f
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-10-17 21:15:21 +0800

    Deal with query params and hash in URL

--- resources/public/status.js
+++ resources/public/status.js
@@ -12,10 +12,14 @@
         .attr('href', 'https://github.com/cburgmer/buildviz')
         .text('buildviz');
 
+    var baseUrl = function () {
+        return window.location.href.replace(new RegExp('/[^/]*([\\?#].*)$'), '');
+    };
+
     var showInitialHelp = function (header) {
         var now = +new Date(),
             aMinuteAgo = now - 61234,
-            serverUrl = window.location.href.replace(new RegExp('/[^/]*$'), ''),
+            serverUrl = baseUrl(),
             commands = ['echo \'{"start":' + aMinuteAgo + ',"end":' + now + ',"outcome":"fail"}\' \\\n  | curl -X PUT -H "Content-type: application/json" -d@- ' + serverUrl + '/builds/my_job/42',
                         'echo \'<testsuite name="my_suite"><testcase classname="my_class" name="my_test" time="0.42"><failure/></testcase></testsuite>\' \\\n  | curl -X PUT -H "Content-type: text/xml" -d@- ' + serverUrl + '/builds/my_job/42/testresults'];
 
