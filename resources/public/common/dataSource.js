31c0d2dccadecf6ed52ee98f746523a2db2d6a5e
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-12-10 22:23:10 +0800

    Load same resources only once per page load, works around Firefox sending second request without cache headers, eases load on the server and improves loading speed

--- resources/public/common/dataSource.js
+++ resources/public/common/dataSource.js
@@ -1,20 +1,50 @@
 var dataSource = function () {
     var module = {};
 
-    module.load = function (url, callback) {
-        d3.json(url, function (error, data) {
-            if (error) return console.warn(error);
+    var loadHandlersByType = {};
 
-            callback(data);
-        });
+    var loadHandlers = function (type) {
+        loadHandlersByType[type] = loadHandlersByType[type] || {};
+        return loadHandlersByType[type];
+    };
+
+    var load = function (type, url, handler) {
+        var handlers = loadHandlers(type)[url] || [];
+        handlers.push(handler);
+        loadHandlers(type)[url] = handlers;
+
+        if (handlers.length === 1) {
+            if (type === 'json') {
+                d3.json(url, function (error, data) {
+                    callbackHandlers(type, url, error, data);
+                });
+            } else {
+                d3.csv(url, function (error, data) {
+                    callbackHandlers(type, url, error, data);
+                });
+            }
+        }
+    };
+
+    var callbackHandlers = function (type, url, error, data) {
+        var handlers = loadHandlers(type)[url];
+        loadHandlers(type)[url] = undefined;
+
+        if (error) {
+            console.warn(error);
+        } else {
+            handlers.forEach(function (handler) {
+                handler(data);
+            });
+        }
+    };
+
+    module.load = function (url, callback) {
+        load('json', url, callback);
     };
 
     module.loadCSV = function (url, callback) {
-        d3.csv(url, function (error, data) {
-            if (error) return console.warn(error);
-
-            callback(data);
-        });
+        load('csv', url, callback);
     };
 
     return module;
