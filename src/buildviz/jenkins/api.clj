2b3b29dfb7594b65b1870adfe60123e02617d151
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-01 21:38:19 +0800

    Sync Jenkins test reports

--- src/buildviz/jenkins/api.clj
+++ src/buildviz/jenkins/api.clj
@@ -1,7 +1,8 @@
 (ns buildviz.jenkins.api
   (:require [cheshire.core :as j]
             [clj-http.client :as client]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.tools.logging :as log]))
 
 (defn- get-json [jenkins-url relative-url]
   (j/parse-string (:body (client/get (string/join [jenkins-url relative-url]))) true))
@@ -11,6 +12,15 @@
     (map :name (get response :jobs))))
 
 (defn get-builds [jenkins-url job-name]
-  (let [response (get-json jenkins-url (format "/job/%s/api/json?pretty=true&tree=builds[number,timestamp,duration,result]" job-name))]
+  (let [response (get-json jenkins-url (format "/job/%s/api/json?tree=builds[number,timestamp,duration,result]" job-name))]
     (->> (get response :builds)
          (map #(assoc % :job-name job-name)))))
+
+(defn get-test-report [jenkins-url job-name build-number]
+  (let [test-report-url (format "/job/%s/%s/testReport/api/json" job-name build-number)]
+    (try
+      (get-json jenkins-url test-report-url)
+      (catch Exception e
+        (if (or (not (ex-data e))
+                (not= 404 (:status (ex-data e))))
+          (log/errorf e "Unable to get test report from %s" test-report-url))))))
--- src/buildviz/jenkins/sync.clj
+++ src/buildviz/jenkins/sync.clj
@@ -5,24 +5,54 @@
             [clojure.string :as string]
             [clojure.tools.logging :as log]))
 
-(defn- jenkins-build->buildviz-build [{:keys [job-name number timestamp duration result]}]
+(defn add-test-results [jenkins-url {:keys [job-name number] :as build}]
+  (assoc build :test-report (api/get-test-report jenkins-url job-name number)))
+
+(defn- jenkins-test-case->buildviz-test-case [{:keys [:className :name :duration :status]}]
+  {:classname className
+   :name name
+   :runtime (Math/round (* duration 1000))
+   :status (case status
+             "PASSED" "pass"
+             "FAILED" "fail"
+             "ERROR" "error"
+             "SKIPPED" "skipped")})
+
+(defn- jenkins-suite->buildviz-suite [{:keys [:name :cases]}]
+  {:name name
+   :children (map jenkins-test-case->buildviz-test-case cases)})
+
+(defn- convert-test-results [test-report]
+  (when test-report
+    (->> (get test-report :suites)
+         (map jenkins-suite->buildviz-suite))))
+
+(defn- jenkins-build->buildviz-build [{:keys [job-name number timestamp duration result test-report]}]
   {:job-name job-name
    :build-id number
    :build {:start timestamp
            :end (+ timestamp duration)
            :outcome (if (= result "SUCCESS")
                       "pass"
-                      "fail")}})
+                      "fail")}
+   :test-results (convert-test-results test-report)})
 
 (defn put-build [buildviz-url job-name build-id build]
   (client/put (string/join [buildviz-url (format "/builds/%s/%s" job-name build-id)])
               {:content-type :json
                :body (j/generate-string build)}))
 
+(defn put-test-results [buildviz-url job-name build-id test-results]
+  (client/put (string/join [buildviz-url (format "/builds/%s/%s/testresults" job-name build-id)])
+              {:content-type :json
+               :body (j/generate-string test-results)}))
 
-(defn put-to-buildviz [buildviz-url {:keys [job-name build-id build]}]
+
+(defn put-to-buildviz [buildviz-url {:keys [job-name build-id build test-results]}]
   (log/info (format "Syncing %s %s: build" job-name build-id))
-  (put-build buildviz-url job-name build-id build))
+  (put-build buildviz-url job-name build-id build)
+  (when test-results
+    (put-test-results buildviz-url job-name build-id test-results)))
 
 
 (defn -main [& c-args]
@@ -32,6 +62,7 @@
 
     (->> (api/get-jobs jenkins-url)
          (mapcat (partial api/get-builds jenkins-url))
+         (map (partial add-test-results jenkins-url))
          (map jenkins-build->buildviz-build)
          (map (partial put-to-buildviz buildviz-url))
          doall)))
