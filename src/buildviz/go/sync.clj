5655747273243901811ddc43ef318ec29f0f7668
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-06 15:39:50 +0800

    Refactor, make eastwood linter happy

--- src/buildviz/go/sync.clj
+++ src/buildviz/go/sync.clj
@@ -20,9 +20,6 @@
 
 (def date-formatter (tf/formatter tz "YYYY-MM-dd" "YYYY/MM/dd" "YYYYMMdd" "dd.MM.YYYY"))
 
-(declare go-url)
-(declare buildviz-url)
-
 (defn usage [options-summary]
   (string/join "\n"
                [""
@@ -60,16 +57,16 @@
       (goaggregate/aggregate-jobs-for-stage stage-instance))))
 
 
-(defn- build-for-job [stage-instance job-name]
+(defn- build-for-job [go-url stage-instance job-name]
   (let [job-instance (assoc stage-instance :job-name job-name)]
     (-> (goapi/build-for go-url job-instance)
         (assoc :name job-name)
         (assoc :junit-xml (goapi/get-junit-xml go-url job-instance)))))
 
-(defn- add-job-instances-for-stage-instance [stage-instance]
+(defn- add-job-instances-for-stage-instance [go-url stage-instance]
   (let [job-names (:job-names stage-instance)]
     (assoc stage-instance
-           :job-instances (map #(build-for-job stage-instance %) job-names))))
+           :job-instances (map #(build-for-job go-url stage-instance %) job-names))))
 
 
 (defn parse-stage-instance [{pipeline-run :pipeline_counter
@@ -82,7 +79,7 @@
    :scheduled-time (tc/from-long (apply min (map :scheduled_date jobs)))
    :job-names (map :name jobs)})
 
-(defn stage-instances-from [sync-start-time {stage-name :stage pipeline-name :pipeline}]
+(defn stage-instances-from [go-url sync-start-time {stage-name :stage pipeline-name :pipeline}]
   (let [safe-build-start-date (t/minus sync-start-time (t/millis 1))]
     (->> (goapi/get-stage-history go-url pipeline-name stage-name)
          (map parse-stage-instance)
@@ -90,7 +87,7 @@
          (take-while #(t/after? (:scheduled-time %) safe-build-start-date)))))
 
 
-(defn add-inputs-for-stage-instance [{:keys [pipeline-run, pipeline-name] :as stage-instance}]
+(defn add-inputs-for-stage-instance [go-url {:keys [pipeline-run, pipeline-name] :as stage-instance}]
   (let [inputs (goapi/get-inputs-for-pipeline-run go-url pipeline-name pipeline-run)]
     (assoc stage-instance :inputs inputs)))
 
@@ -123,24 +120,24 @@
                   :inputs inputs}})
        job-instances))
 
-(defn buildviz-build-base-url [job-name build-no]
+(defn buildviz-build-base-url [buildviz-url job-name build-no]
   (format "%s/builds/%s/%s" buildviz-url job-name build-no))
 
-(defn put-build [job-name build-no build]
-  (client/put (buildviz-build-base-url job-name build-no)
+(defn put-build [buildviz-url job-name build-no build]
+  (client/put (buildviz-build-base-url buildviz-url job-name build-no)
               {:content-type :json
                :body (j/generate-string build)}))
 
-(defn put-junit-xml [job-name build-no xml-content]
-  (client/put (clojure.string/join [(buildviz-build-base-url job-name build-no) "/testresults"])
+(defn put-junit-xml [buildviz-url job-name build-no xml-content]
+  (client/put (clojure.string/join [(buildviz-build-base-url buildviz-url job-name build-no) "/testresults"])
               {:body xml-content}))
 
-(defn put-to-buildviz [{job-name :job-name build-no :build-id build :build junit-xml :junit-xml}]
+(defn put-to-buildviz [buildviz-url {job-name :job-name build-no :build-id build :build junit-xml :junit-xml}]
   (log/info (format "Syncing %s %s: build" job-name build-no))
-  (put-build job-name build-no build)
+  (put-build buildviz-url job-name build-no build)
   (when (some? junit-xml)
     (try
-      (put-junit-xml job-name build-no junit-xml)
+      (put-junit-xml buildviz-url job-name build-no junit-xml)
       (catch Exception e
         (if-let [data (ex-data e)]
           (do
@@ -152,22 +149,22 @@
 
 (def last-week (t/minus (.withTimeAtStartOfDay (l/local-now)) (t/weeks 1)))
 
-(defn- get-latest-synced-build-start []
+(defn- get-latest-synced-build-start [buildviz-url]
   (let [response (client/get (format "%s/status" buildviz-url))
         buildviz-status (j/parse-string (:body response) true)]
     (when-let [latest-build-start (:latestBuildStart buildviz-status)]
       (tc/from-long latest-build-start))))
 
-(defn- get-start-date [date-from-config]
+(defn- get-start-date [buildviz-url date-from-config]
   (if (some? date-from-config)
     date-from-config
-    (if-let [latest-sync-build-start (get-latest-synced-build-start)]
+    (if-let [latest-sync-build-start (get-latest-synced-build-start buildviz-url)]
       latest-sync-build-start
       last-week)))
 
 ;; run
 
-(defn- emit-start [sync-start-time pipeline-stages]
+(defn- emit-start [go-url buildviz-url sync-start-time pipeline-stages]
   (println "Go" go-url (distinct (map :group pipeline-stages)) "-> buildviz" buildviz-url)
   (print (format "Finding all pipeline runs for syncing (starting from %s)..."
                  (tf/unparse (:date-time tf/formatters) sync-start-time)))
@@ -179,34 +176,35 @@
   (println "done")
   pipeline-stages)
 
+(defn- sync-stages [go-url buildviz-url sync-start-time sync-jobs-for-pipelines selected-pipeline-group-names]
+  (->> (goapi/get-stages go-url)
+       (select-stages selected-pipeline-group-names)
+       (emit-start go-url buildviz-url sync-start-time)
+       (mapcat #(stage-instances-from go-url sync-start-time %))
+       (sort-by :scheduled-time)
+       (take-while :finished)
+       (emit-sync-start)
+       (progress/init "Syncing")
+       (map #(add-inputs-for-stage-instance go-url %))
+       (map #(add-job-instances-for-stage-instance go-url %))
+       (map #(aggregate-jobs-for-stage-instance % sync-jobs-for-pipelines))
+       (mapcat stage-instances->builds)
+       (map #(put-to-buildviz buildviz-url %))
+       (map progress/tick)
+       dorun
+       (progress/done)))
+
 (defn -main [& c-args]
-  (def args (parse-opts c-args cli-options))
+  (let [args (parse-opts c-args cli-options)]
+    (when (or (:help (:options args))
+              (empty? (:arguments args)))
+      (println (usage (:summary args)))
+      (System/exit 0))
 
-  (when (or (:help (:options args))
-            (empty? (:arguments args)))
-    (println (usage (:summary args)))
-    (System/exit 0))
+    (let [go-url (first (:arguments args))
+          buildviz-url (:buildviz-url (:options args))
+          sync-start-time (get-start-date buildviz-url (:sync-start-time (:options args)))
+          sync-jobs-for-pipelines (set (:sync-jobs-for-pipelines (:options args)))
+          selected-pipeline-group-names (set (:pipeline-groups (:options args)))]
 
-  (def go-url (first (:arguments args)))
-  (def buildviz-url (:buildviz-url (:options args)))
-
-  (let [sync-start-time (get-start-date (:sync-start-time (:options args)))
-        sync-jobs-for-pipelines (set (:sync-jobs-for-pipelines (:options args)))
-        selected-pipeline-group-names (set (:pipeline-groups (:options args)))]
-
-    (->> (goapi/get-stages go-url)
-         (select-stages selected-pipeline-group-names)
-         (emit-start sync-start-time)
-         (mapcat #(stage-instances-from sync-start-time %))
-         (sort-by :scheduled-time)
-         (take-while :finished)
-         (emit-sync-start)
-         (progress/init "Syncing")
-         (map add-inputs-for-stage-instance)
-         (map add-job-instances-for-stage-instance)
-         (map #(aggregate-jobs-for-stage-instance % sync-jobs-for-pipelines))
-         (mapcat stage-instances->builds)
-         (map put-to-buildviz)
-         (map progress/tick)
-         dorun
-         (progress/done))))
+      (sync-stages go-url buildviz-url sync-start-time sync-jobs-for-pipelines selected-pipeline-group-names))))
