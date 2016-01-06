962bb340af3005fa8dc18ee0123825ea5cffd552
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-06 22:38:38 +0800

    Average pipeline runtimes by day

--- src/buildviz/analyse/pipelines.clj
+++ src/buildviz/analyse/pipelines.clj
@@ -1,5 +1,10 @@
 (ns buildviz.analyse.pipelines
-  (:require [buildviz.data.build-schema :refer [was-triggered-by?]]))
+  (:require [buildviz.data.build-schema :refer [was-triggered-by?]]
+            [buildviz.util.math :refer [avg]]
+            [clj-time
+             [coerce :as tc]
+             [core :as t]
+             [format :as tf]]))
 
 (defn- find-build [{:keys [job-name build-id]} builds]
   (when job-name
@@ -21,9 +26,44 @@
         (recur (cons triggering-build pipeline))
         pipeline))))
 
-(defn find-pipelines [builds]
+(defn find-pipeline-runs [builds]
   (let [pipeline-end-candidates (filter #(is-pipeline-end? % builds) builds)]
     (->> pipeline-end-candidates
          (map #(find-pipeline-ending-with % builds))
-         (filter #(< 1 (count %)))
-         (group-by #(map :job %)))))
+         (filter #(< 1 (count %))))))
+
+
+(def date-only-formatter (tf/formatter "yyyy-MM-dd" (t/default-time-zone)))
+
+(defn- date-for [timestamp]
+  (when timestamp
+    (tf/unparse date-only-formatter (tc/from-long (long timestamp)))))
+
+(defn- pipeline-run-end [builds-of-pipeline-run]
+  (:end (last builds-of-pipeline-run)))
+
+(defn- total-runtime [builds-of-pipeline-run]
+  (when-let [end (pipeline-run-end builds-of-pipeline-run)]
+    (let [start (:start (first builds-of-pipeline-run))]
+      (- end start))))
+
+(defn- total-pipeline-runtime-by-day [pipeline-runs]
+  (->> pipeline-runs
+       (map (fn [builds-of-pipeline-run]
+              [(date-for (pipeline-run-end builds-of-pipeline-run))
+               (total-runtime builds-of-pipeline-run)]))
+       (filter first)
+       (group-by first)
+       (map (fn [[k pipeline-runtimes]]
+              [k (->> pipeline-runtimes
+                      (map second)
+                      avg)]))
+       (into {})))
+
+(defn pipeline-runtimes-by-day [builds]
+  (->> builds
+       find-pipeline-runs
+       (group-by #(map :job %))
+       (map (fn [[k pipeline-runs]]
+              [k (total-pipeline-runtime-by-day pipeline-runs)]))
+       (into {})))
--- test/buildviz/analyse/pipelines_test.clj
+++ test/buildviz/analyse/pipelines_test.clj
@@ -2,88 +2,103 @@
   (:require [buildviz.analyse.pipelines :as sut]
             [clojure.test :refer :all]))
 
+(def a-day (* 24 60 60 1000))
+
 (deftest test-build-pipelines
   (testing "should find a chain of two builds"
     (is (= [["test" "deploy"]]
-           (keys (sut/find-pipelines [{:job "deploy"
-                                       :build-id "42"
-                                       :triggered-by {:job-name "test"
-                                                      :build-id "41"}}
-                                      {:job "test"
-                                       :build-id "41"}])))))
+           (keys (sut/pipeline-runtimes-by-day [{:job "deploy"
+                                                 :build-id "42"
+                                                 :triggered-by {:job-name "test"
+                                                                :build-id "41"}}
+                                                {:job "test"
+                                                 :build-id "41"}])))))
 
   (testing "should find chain of 3 builds"
     (is (= [["test" "deploy-staging" "deploy-live"]]
-           (keys (sut/find-pipelines [{:job "deploy-live"
-                                       :build-id "42"
-                                       :triggered-by {:job-name "deploy-staging"
-                                                      :build-id "42"}}
-                                      {:job "deploy-staging"
-                                       :build-id "42"
-                                       :triggered-by {:job-name "test"
-                                                      :build-id "41"}}
-                                      {:job "test"
-                                       :build-id "41"}])))))
+           (keys (sut/pipeline-runtimes-by-day [{:job "deploy-live"
+                                                 :build-id "42"
+                                                 :triggered-by {:job-name "deploy-staging"
+                                                                :build-id "42"}}
+                                                {:job "deploy-staging"
+                                                 :build-id "42"
+                                                 :triggered-by {:job-name "test"
+                                                                :build-id "41"}}
+                                                {:job "test"
+                                                 :build-id "41"}])))))
 
   (testing "should handle shared chain"
     (is (= [["test" "deploy-qa"]
             ["test" "deploy-uat"]]
-           (keys (sut/find-pipelines [{:job "test"
-                                       :build-id 41}
-                                      {:job "deploy-qa"
-                                       :triggered-by {:job-name "test"
-                                                      :build-id 41}}
-                                      {:job "deploy-uat"
-                                       :triggered-by {:job-name "test"
-                                                      :build-id 41}}])))))
+           (keys (sut/pipeline-runtimes-by-day [{:job "test"
+                                                 :build-id 41}
+                                                {:job "deploy-qa"
+                                                 :triggered-by {:job-name "test"
+                                                                :build-id 41}}
+                                                {:job "deploy-uat"
+                                                 :triggered-by {:job-name "test"
+                                                                :build-id 41}}])))))
 
   (testing "should handle missing triggering build"
     (is (= [["deploy-staging" "deploy-live"]]
-           (keys (sut/find-pipelines [{:job "deploy-live"
-                                       :build-id "42"
-                                       :triggered-by {:job-name "deploy-staging"
-                                                      :build-id "42"}}
-                                      {:job "deploy-staging"
-                                       :build-id "42"
-                                       :triggered-by {:job-name "test"
-                                                      :build-id "41"}}])))))
+           (keys (sut/pipeline-runtimes-by-day [{:job "deploy-live"
+                                                 :build-id "42"
+                                                 :triggered-by {:job-name "deploy-staging"
+                                                                :build-id "42"}}
+                                                {:job "deploy-staging"
+                                                 :build-id "42"
+                                                 :triggered-by {:job-name "test"
+                                                                :build-id "41"}}])))))
 
   (testing "should ignore 'stand-alone' builds"
     (is (= {}
-           (sut/find-pipelines [{:job "deploy"
-                                 :build-id "42"}
-                                {:job "test"
-                                 :build-id "41"}]))))
+           (sut/pipeline-runtimes-by-day [{:job "deploy"
+                                           :build-id "42"}
+                                          {:job "test"
+                                           :build-id "41"}]))))
 
   (testing "should ignore 1-build pipeline due to missing triggering build"
     (is (= {}
-           (sut/find-pipelines [{:job "deploy"
-                                 :build-id "42"
-                                 :triggered-by {:job-name "test"
-                                                :build-id "40"}}]))))
+           (sut/pipeline-runtimes-by-day [{:job "deploy"
+                                           :build-id "42"
+                                           :triggered-by {:job-name "test"
+                                                          :build-id "40"}}]))))
 
   (testing "should aggregate multiple instances of a pipeline run"
-    (is (= {["test" "deploy"] [[{:job "test"
-                                 :build-id "41"}
-                                {:job "deploy"
-                                 :build-id "42"
-                                 :triggered-by {:job-name "test"
-                                                :build-id "41"}}]
-                               [{:job "test"
-                                 :build-id "40"}
-                                {:job "deploy"
-                                 :build-id "41"
-                                 :triggered-by {:job-name "test"
-                                                :build-id "40"}}]]}
-           (sut/find-pipelines [{:job "deploy"
-                                 :build-id "42"
-                                 :triggered-by {:job-name "test"
-                                                :build-id "41"}}
-                                {:job "test"
-                                 :build-id "41"}
-                                {:job "deploy"
-                                 :build-id "41"
-                                 :triggered-by {:job-name "test"
-                                                :build-id "40"}}
-                                {:job "test"
-                                 :build-id "40"}])))))
+    (is (= {["test" "deploy"] {"1970-01-01" 600
+                               "1970-01-02" 800}}
+           (sut/pipeline-runtimes-by-day [{:job "deploy"
+                                           :build-id "42"
+                                           :triggered-by {:job-name "test"
+                                                          :build-id "41"}
+                                           :end (+ 1000 a-day)}
+                                          {:job "test"
+                                           :build-id "41"
+                                           :start (+ 200 a-day)}
+                                          {:job "deploy"
+                                           :build-id "41"
+                                           :triggered-by {:job-name "test"
+                                                          :build-id "40"}
+                                           :end 1000}
+                                          {:job "test"
+                                           :build-id "40"
+                                           :start 400}]))))
+
+  (testing "should average multiple runs on one day"
+    (is (= {["test" "deploy"] {"1970-01-01" 700}}
+           (sut/pipeline-runtimes-by-day [{:job "deploy"
+                                           :build-id "42"
+                                           :triggered-by {:job-name "test"
+                                                          :build-id "41"}
+                                           :end 1000}
+                                          {:job "test"
+                                           :build-id "41"
+                                           :start 200}
+                                          {:job "deploy"
+                                           :build-id "41"
+                                           :triggered-by {:job-name "test"
+                                                          :build-id "40"}
+                                           :end 1000}
+                                          {:job "test"
+                                           :build-id "40"
+                                           :start 400}])))))
