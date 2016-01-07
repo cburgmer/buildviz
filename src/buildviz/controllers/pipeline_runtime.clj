7796756c2a3f9575471eab282e7976828b114470
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-07 21:21:31 +0800

    Accept CSV for pipeline runtime and support time offset

--- src/buildviz/controllers/pipeline_runtime.clj
+++ src/buildviz/controllers/pipeline_runtime.clj
@@ -1,7 +1,35 @@
 (ns buildviz.controllers.pipeline-runtime
   (:require [buildviz.analyse.pipelines :refer [pipeline-runtimes-by-day]]
             [buildviz.data.results :as results]
-            [buildviz.util.http :as http]))
+            [buildviz.util.csv :as csv]
+            [buildviz.util.http :as http]
+            [clojure.string :as str]))
+
+(defn- remap-date-first [[key runtimes-by-day]]
+  (map (fn [[day avg-runtime]]
+         [day {key avg-runtime}])
+       runtimes-by-day))
+
+(defn- merge-runtimes [all-runtimes-by-day]
+  (->> (mapcat remap-date-first all-runtimes-by-day)
+       (group-by first)
+       (map (fn [[date entries]]
+              [date (apply merge (map second entries))]))))
+
+(defn- serialize-pipelines [pipelines]
+  (map #(str/join "|" %) pipelines))
+
+(defn- runtime-of-day-row [pipelines [date runtimes-by-pipeline]]
+  (->> (map #(get runtimes-by-pipeline %) pipelines)
+       (map csv/format-duration)
+       (cons date)))
+
+(defn- pipeline-runtime-as-table [pipeline-runtimes]
+  (let [pipelines (keys pipeline-runtimes)]
+    (csv/export-table (cons "date" (serialize-pipelines pipelines))
+                      (->> (merge-runtimes pipeline-runtimes)
+                           (sort-by first)
+                           (map #(runtime-of-day-row pipelines %))))))
 
 (defn- runtimes-as-list [runtime-by-day]
   (map (fn [[date runtime]]
@@ -9,9 +37,11 @@
           :runtime runtime})
        runtime-by-day))
 
-(defn get-pipeline-runtime [build-results]
-  (http/respond-with-json (->> (results/all-builds build-results 0)
-                               pipeline-runtimes-by-day
-                               (map (fn [[job-names runtime-by-day]]
-                                      {:pipeline job-names
-                                       :runtimes (runtimes-as-list runtime-by-day)})))))
+(defn get-pipeline-runtime [build-results accept from]
+  (let [pipeline-runtimes (pipeline-runtimes-by-day (results/all-builds build-results from))]
+    (if (= (:mime accept) :json)
+      (http/respond-with-json (->> pipeline-runtimes
+                                   (map (fn [[job-names runtime-by-day]]
+                                          {:pipeline job-names
+                                           :runtimes (runtimes-as-list runtime-by-day)}))))
+      (http/respond-with-csv (pipeline-runtime-as-table pipeline-runtimes)))))
--- test/buildviz/controllers/pipeline_runtime_test.clj
+++ test/buildviz/controllers/pipeline_runtime_test.clj
@@ -1,7 +1,13 @@
 (ns buildviz.controllers.pipeline-runtime-test
   (:require [buildviz.controllers.pipeline-runtime :as sut]
-            [buildviz.test-utils :refer [json-body json-get-request the-app]]
-            [clojure.test :refer :all]))
+            [buildviz.test-utils
+             :refer
+             [json-body json-get-request plain-get-request the-app]]
+            [clojure
+             [string :as str]
+             [test :refer :all]]))
+
+(def a-day (* 24 60 60 1000))
 
 (deftest test-get-pipeline-runtime
   (testing "should return pipeline runtime"
@@ -19,4 +25,39 @@
       (is (= [{"pipeline" ["test" "deploy"]
                "runtimes" [{"date" "1970-01-01"
                             "runtime" 700}]}]
-             (json-body (json-get-request app "/pipelineruntime")))))))
+             (json-body (json-get-request app "/pipelineruntime"))))))
+
+  (testing "should respect time offset"
+    (let [app (the-app {"test" {1 {:start 100}
+                                2 {:start (+ 2000 a-day)}}
+                        "deploy" {1 {:start 200
+                                     :end 700
+                                     :triggered-by {:job-name "test"
+                                                    :build-id 1}}
+                                  2 {:start (+ 2500 a-day)
+                                     :end (+ 2800 a-day)
+                                     :triggered-by {:job-name "test"
+                                                    :build-id 2}}}}
+                       {})]
+      (is (= [{"pipeline" ["test" "deploy"]
+               "runtimes" [{"date" "1970-01-02"
+                            "runtime" 800}]}]
+             (json-body (json-get-request app "/pipelineruntime" {"from" a-day}))))))
+
+  (testing "should respond with CSV"
+    (let [app (the-app {"test" {1 {:start 100}
+                                2 {:start (+ 2000 a-day)}}
+                        "deploy-staging" {1 {:start 200
+                                             :end 700
+                                             :triggered-by {:job-name "test"
+                                                            :build-id 1}}}
+                        "deploy-uat" {2 {:start (+ 2500 a-day)
+                                         :end (+ 2800 a-day)
+                                         :triggered-by {:job-name "test"
+                                                        :build-id 2}}}}
+                       {})]
+      (is (= (str/join "\n" ["date,test|deploy-staging,test|deploy-uat"
+                             (format "1970-01-01,%.8f," (float (/ 600 a-day)))
+                             (format "1970-01-02,,%.8f" (float (/ 800 a-day)))
+                             ""])
+             (:body (plain-get-request app "/pipelineruntime")))))))
