6d19e835e900adb549aca0109ce571b39d3108aa
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-08 22:51:47 +0800

    Improve timespan selection

--- resources/public/common/timespanSelection.js
+++ resources/public/common/timespanSelection.js
@@ -12,26 +12,32 @@ var timespanSelection = function (d3) {
     module.timespans = {
         all: {
             label: 'all time',
+            shortLabel: 'all time',
             timestamp: function () { return 0; }
         },
         twoMonths: {
             label: 'last two months',
+            shortLabel: '2 months',
             timestamp: fromDaysAgo(60)
         },
         twoWeeks: {
             label: 'last two weeks',
+            shortLabel: '2 weeks',
             timestamp: fromDaysAgo(14)
         },
         sevenDays: {
             label: 'last 7 days',
+            shortLabel: '7 days',
             timestamp: fromDaysAgo(7)
         },
         twoDays: {
             label: 'last 2 days',
+            shortLabel: '2 days',
             timestamp: fromDaysAgo(2)
         },
         today: {
             label: 'today',
+            shortLabel: 'today',
             timestamp: fromDaysAgo(1)
         }
     };
@@ -47,27 +53,48 @@ var timespanSelection = function (d3) {
                 .attr('class', 'timespan');
 
         var currentlySelected = container.append('span')
-            .text(selectedSpan.label);
+                .on('click', function () {
+                    d3.event.preventDefault();
+                });
 
         var timespanList = container.append('div')
                 .attr('class', 'timespanSelection')
-                .text('Aggregate over data from')
                 .append('ol')
                 .attr('class', 'timespanList');
 
-        Object.keys(module.timespans).forEach(function (spanName) {
-            var span = module.timespans[spanName];
-
-            timespanList.append('li')
-                .attr('class', 'item')
-                .append("button")
-                .text(span.label)
-                .on('click', function () {
-                    currentlySelected.text(span.label);
-                    onTimespanSelected(span);
-                    d3.event.preventDefault();
-                });
+        var timespans = Object.keys(module.timespans).map(function (spanName) {
+            return module.timespans[spanName];
         });
+
+        var selection = timespanList.selectAll(".item")
+                .data(timespans,
+                      function (d) {
+                          return d.label;
+                      });
+
+        var updateSelection = function () {
+            currentlySelected.text(selectedSpan.shortLabel);
+            selection
+                .classed('selected', function (span) {
+                    return span === selectedSpan;
+                });
+        };
+
+        selection.enter()
+            .append('li')
+            .attr('class', 'item')
+            .append("button")
+            .text(function (span) {
+                return span.label;
+            })
+            .on('click', function (span) {
+                selectedSpan = span;
+                updateSelection();
+                onTimespanSelected(span);
+                d3.event.preventDefault();
+            });
+
+        updateSelection();
 
         return {
             widget: container.node(),
