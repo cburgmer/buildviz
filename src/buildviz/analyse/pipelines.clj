3c645c2c076e10cd5edf5bff1bcaddd888a93004
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-24 15:07:40 +0800

    Refactor pipeline runtime

--- src/buildviz/analyse/pipelines.clj
+++ src/buildviz/analyse/pipelines.clj
@@ -41,38 +41,53 @@
   (when timestamp
     (tf/unparse date-only-formatter (tc/from-long (long timestamp)))))
 
+(defn- average-duration [builds]
+  (avg (map :duration builds)))
+
+(defn- average-duration-by-day [duration-entries]
+  (->> duration-entries
+       (group-by #(date-for (:end %)))
+       (filter first)
+       (map (fn [[date builds]]
+              [date (average-duration builds)]))
+       (into {})))
+
+(defn- average-by-day [duration-entries]
+  (->> duration-entries
+       (group-by :name)
+       (map (fn [[name duration-entries]]
+              [name (average-duration-by-day duration-entries)]))
+       (into {})))
+
+
 (defn- pipeline-run-end [builds-of-pipeline-run]
   (:end (last builds-of-pipeline-run)))
 
+(defn- pipeline-run-start [builds-of-pipeline-run]
+  (:start (first builds-of-pipeline-run)))
+
+(defn- pipeline-run-outcome [builds-of-pipeline-run]
+  (:outcome (last builds-of-pipeline-run)))
+
+(defn- pipeline-run-name [builds-of-pipeline-run]
+  (map :job builds-of-pipeline-run))
+
 (defn- total-runtime [builds-of-pipeline-run]
   (when-let [end (pipeline-run-end builds-of-pipeline-run)]
-    (let [start (:start (first builds-of-pipeline-run))]
-      (- end start))))
-
-(defn- total-pipeline-runtime-by-day [pipeline-runs]
-  (->> pipeline-runs
-       (map (fn [builds-of-pipeline-run]
-              [(date-for (pipeline-run-end builds-of-pipeline-run))
-               (total-runtime builds-of-pipeline-run)]))
-       (filter first)
-       (group-by first)
-       (map (fn [[k pipeline-runtimes]]
-              [k (->> pipeline-runtimes
-                      (map second)
-                      avg)]))
-       (into {})))
+    (- end
+       (pipeline-run-start builds-of-pipeline-run))))
 
 (defn- ignore-unsuccessful-pipeline-runs-to-remove-noise-of-interrupted-pipelines [pipeline-runs]
   (remove (fn [pipeline-run]
-            (let [pipeline-end-build (last pipeline-run)]
-              (= "fail" (:outcome pipeline-end-build))))
+            (= "fail" (pipeline-run-outcome pipeline-run)))
           pipeline-runs))
 
 (defn pipeline-runtimes-by-day [builds]
   (->> builds
        find-pipeline-runs
        ignore-unsuccessful-pipeline-runs-to-remove-noise-of-interrupted-pipelines
-       (group-by #(map :job %))
-       (map (fn [[k pipeline-runs]]
-              [k (total-pipeline-runtime-by-day pipeline-runs)]))
-       (into {})))
+       (map (fn [pipeline-run]
+              {:name (pipeline-run-name pipeline-run)
+               :end (pipeline-run-end pipeline-run)
+               :duration (total-runtime pipeline-run)}))
+       average-by-day))
