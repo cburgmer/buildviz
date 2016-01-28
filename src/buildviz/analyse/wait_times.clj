bf4d93f09fc0808dd952d386d71c2256900207e0
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-28 23:22:34 +0800

    Handle multiple triggering builds for calculating wait times

--- src/buildviz/analyse/wait_times.clj
+++ src/buildviz/analyse/wait_times.clj
@@ -1,29 +1,31 @@
 (ns buildviz.analyse.wait-times
   (:require [buildviz.analyse.duration :as duration]))
 
-(defn- for-now-lets-ignore-a-build-can-be-triggered-by-multiple-builds [triggered-by]
-  (first triggered-by))
-
 (defn- find-build [{:keys [job-name build-id]} builds]
-  (when job-name
-    (first (filter (fn [{that-job-name :job that-build-id :build-id}]
-                     (and (= job-name that-job-name)
-                          (= build-id that-build-id)))
-                   builds))))
+  (first (filter (fn [{that-job-name :job that-build-id :build-id}]
+                   (and (= job-name that-job-name)
+                        (= build-id that-build-id)))
+                 builds)))
 
-(defn- build-wait-time [{:keys [job start end triggered-by]} builds]
-  (when-let [triggering-build-end (:end (find-build (for-now-lets-ignore-a-build-can-be-triggered-by-multiple-builds triggered-by) builds))]
+(defn- build-wait-time [{:keys [job start end]} triggering-build]
+  (when-let [triggering-build-end (:end triggering-build)]
     (let [wait-time (- start
                        triggering-build-end)]
       {:name job
        :end end
        :duration wait-time})))
 
+(defn- build-wait-times [build all-builds]
+  (->> (:triggered-by build)
+       (map #(find-build % all-builds))
+       (remove nil?)
+       (map #(build-wait-time build %))))
+
 (defn- wait-times [builds]
   (->> builds
        (filter :triggered-by)
        (filter :end)
-       (map #(build-wait-time % builds))
+       (mapcat #(build-wait-times % builds))
        (remove nil?)))
 
 
--- test/buildviz/analyse/wait_times_test.clj
+++ test/buildviz/analyse/wait_times_test.clj
@@ -11,46 +11,62 @@
    :start 0})
 
 (defn- a-triggered-build
-  ([job start triggering-job triggering-build-id]
-   (a-triggered-build job "42" start triggering-job triggering-build-id))
-  ([job build-id start triggering-job triggering-build-id]
+  ([job start triggered-by]
+   (a-triggered-build job "42" start triggered-by))
+  ([job build-id start triggered-by]
    {:job job
     :build-id build-id
     :start start
     :end (+ start 1000)
-    :triggered-by [{:job-name triggering-job
-                    :build-id triggering-build-id}]}))
+    :triggered-by (map (fn [[triggering-job triggering-build-id]]
+                         {:job-name triggering-job
+                          :build-id triggering-build-id})
+                       triggered-by)}))
 
 (deftest test-wait-times-by-day
   (testing "should calculate wait time for triggered build"
     (is (= {"deploy" {"1970-01-01" 800}}
-           (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")
+           (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 [["test" "41"]])
                                    (a-build "test" "41" 200)]))))
 
+  (testing "should calculate wait time for build triggered by two"
+    (is (= {"deploy" {"1970-01-01" 700}}
+           (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 [["test" "41"]
+                                                                     ["test" "40"]])
+                                   (a-build "test" "41" 500)
+                                   (a-build "test" "40" 100)]))))
+
   (testing "should handle optional 'end' value for triggering build"
-    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")
+    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 [["test" "41"]])
                                         (-> (a-build "test" "41" 200)
                                             (dissoc :end))]))))
 
   (testing "should handle optional 'end' value for triggered build"
-    (is (empty? (sut/wait-times-by-day [(-> (a-triggered-build "deploy" 1000 "test" "41")
+    (is (empty? (sut/wait-times-by-day [(-> (a-triggered-build "deploy" 1000 [["test" "41"]])
                                             (dissoc :end))
                                         (a-build "test" "41" 200)]))))
 
   (testing "should handle missing triggering build"
-    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")]))))
+    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 [["test" "41"]])]))))
 
   (testing "should average wait times"
     (is (= {"deploy" {"1970-01-01" 900}}
-           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" 2000 "test" "41")
+           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" 2000 [["test" "41"]])
                                    (a-build "test" "41" 1000)
-                                   (a-triggered-build "deploy" "30" 1000 "test" "30")
+                                   (a-triggered-build "deploy" "30" 1000 [["test" "30"]])
                                    (a-build "test" "30" 200)]))))
 
   (testing "should list wait times by day"
     (is (= {"deploy" {"1970-01-01" 800
                       "1970-01-02" 1000}}
-           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" (+ 2000 a-day) "test" "41")
+           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" (+ 2000 a-day) [["test" "41"]])
                                    (a-build "test" "41" (+ 1000 a-day))
-                                   (a-triggered-build "deploy" "30" 1000 "test" "30")
-                                   (a-build "test" "30" 200)])))))
+                                   (a-triggered-build "deploy" "30" 1000 [["test" "30"]])
+                                   (a-build "test" "30" 200)]))))
+
+  (testing "should calculate wait time for build triggered by two over two days"
+    (is (= {"deploy" {"1970-01-02" (/ a-day 2)}}
+           (sut/wait-times-by-day [(a-triggered-build "deploy" a-day [["test" "41"]
+                                                                      ["test" "40"]])
+                                   (a-build "test" "41" a-day)
+                                   (a-build "test" "40" 0)])))))
