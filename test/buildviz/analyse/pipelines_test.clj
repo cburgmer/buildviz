7dc757daa6e6561eb932b8ef47fcec8577c64c65
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-01-28 21:37:16 +0800

    Refactor test, generate test data so we make sure tests check actual behaviour, and not data side effects

--- test/buildviz/analyse/pipelines_test.clj
+++ test/buildviz/analyse/pipelines_test.clj
@@ -4,92 +4,72 @@
 
 (def a-day (* 24 60 60 1000))
 
+(defn- a-build [job build-id]
+  {:job job
+   :build-id build-id
+   :start 0})
+
+(defn- a-triggered-build
+  ([job triggering-job triggering-build-id]
+   (a-triggered-build job "21" triggering-job triggering-build-id))
+  ([job build-id triggering-job triggering-build-id]
+   {:job job
+    :build-id build-id
+    :start 0
+    :end 10
+    :triggered-by {:job-name triggering-job
+                   :build-id triggering-build-id}}))
+
 (deftest test-build-pipelines
   (testing "should find a chain of two builds"
     (is (= [["test" "deploy"]]
-           (keys (sut/pipeline-runtimes-by-day [{:job "deploy"
-                                                 :end 10
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id "41"}}
-                                                {:job "test"
-                                                 :build-id "41"
-                                                 :start 0}])))))
+           (keys (sut/pipeline-runtimes-by-day [(a-triggered-build "deploy" "test" "41")
+                                                (a-build "test" "41")])))))
 
   (testing "should find chain of 3 builds"
     (is (= [["test" "deploy-staging" "deploy-live"]]
-           (keys (sut/pipeline-runtimes-by-day [{:job "deploy-live"
-                                                 :end 10
-                                                 :triggered-by {:job-name "deploy-staging"
-                                                                :build-id "42"}}
-                                                {:job "deploy-staging"
-                                                 :build-id "42"
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id "41"}}
-                                                {:job "test"
-                                                 :build-id "41"
-                                                 :start 0}])))))
+           (keys (sut/pipeline-runtimes-by-day [(a-triggered-build "deploy-live" "deploy-staging" "42")
+                                                (a-triggered-build "deploy-staging" "42" "test" "41")
+                                                (a-build "test" "41")])))))
 
   (testing "should handle shared chain"
     (is (= [["test" "deploy-qa"]
             ["test" "deploy-uat"]]
-           (keys (sut/pipeline-runtimes-by-day [{:job "test"
-                                                 :build-id 41
-                                                 :start 0}
-                                                {:job "deploy-qa"
-                                                 :end 10
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id 41}}
-                                                {:job "deploy-uat"
-                                                 :end 10
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id 41}}])))))
+           (keys (sut/pipeline-runtimes-by-day [(a-build "test" "41")
+                                                (a-triggered-build "deploy-qa" "test" "41")
+                                                (a-triggered-build "deploy-uat" "test" "41")])))))
 
   (testing "should handle missing triggering build"
     (is (= [["deploy-staging" "deploy-live"]]
-           (keys (sut/pipeline-runtimes-by-day [{:job "deploy-live"
-                                                 :end 10
-                                                 :triggered-by {:job-name "deploy-staging"
-                                                                :build-id "42"}}
-                                                {:job "deploy-staging"
-                                                 :build-id "42"
-                                                 :start 0
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id "41"}}])))))
+           (keys (sut/pipeline-runtimes-by-day [(a-triggered-build "deploy-live" "deploy-staging" "42")
+                                                (a-triggered-build "deploy-staging" "42" "test" "41")])))))
 
   (testing "should ignore 'stand-alone' builds"
     (is (= {}
-           (sut/pipeline-runtimes-by-day [{:job "deploy"
-                                           :build-id "42"}
-                                          {:job "test"
-                                           :build-id "41"}]))))
+           (sut/pipeline-runtimes-by-day [(a-build "deploy" "42")
+                                          (a-build "test" "41")]))))
 
   (testing "should ignore 1-build pipeline due to missing triggering build"
     (is (= {}
-           (sut/pipeline-runtimes-by-day [{:job "deploy"
-                                           :build-id "42"
-                                           :triggered-by {:job-name "test"
-                                                          :build-id "40"}}]))))
+           (sut/pipeline-runtimes-by-day [(a-triggered-build "deploy" "test" "41")]))))
 
   (testing "should ignore pipeline failing in last step"
     (is (= {}
-           (sut/pipeline-runtimes-by-day [{:job "deploy"
-                                           :build-id "42"
-                                           :triggered-by {:job-name "test"
-                                                          :build-id "41"}
-                                           :outcome "fail"}
-                                          {:job "test"
-                                           :build-id "41"}]))))
+           (sut/pipeline-runtimes-by-day [(-> (a-triggered-build "deploy" "test" "41")
+                                              (assoc :outcome "fail"))
+                                          (a-build "test" "41")]))))
 
   (testing "should include pipeline successful in last step"
     (is (= [["test" "deploy"]]
-           (keys (sut/pipeline-runtimes-by-day [{:job "deploy"
-                                                 :end 10
-                                                 :triggered-by {:job-name "test"
-                                                                :build-id "41"}
-                                                 :outcome "pass"}
-                                                {:job "test"
-                                                 :build-id "41"
-                                                 :start 0}])))))
+           (keys (sut/pipeline-runtimes-by-day [(-> (a-triggered-build "deploy" "test" "41")
+                                                    (assoc :outcome "pass"))
+                                                (a-build "test" "41")])))))
+
+  (testing "should handle missing end time"
+    (is (= {}
+           (sut/pipeline-runtimes-by-day [(-> (a-triggered-build "deploy" "test" "41")
+                                              (dissoc :end))
+                                          (a-build "test" "41")]))))
 
   (testing "should aggregate multiple instances of a pipeline run"
     (is (= {["test" "deploy"] {"1970-01-01" 600
--- test/buildviz/analyse/wait_times_test.clj
+++ test/buildviz/analyse/wait_times_test.clj
@@ -4,86 +4,53 @@
 
 (def a-day (* 24 60 60 1000))
 
+(defn- a-build [job build-id end]
+  {:job job
+   :build-id build-id
+   :end end
+   :start 0})
+
+(defn- a-triggered-build
+  ([job start triggering-job triggering-build-id]
+   (a-triggered-build job "42" start triggering-job triggering-build-id))
+  ([job build-id start triggering-job triggering-build-id]
+   {:job job
+    :build-id build-id
+    :start start
+    :end (+ start 1000)
+    :triggered-by {:job-name triggering-job
+                   :build-id triggering-build-id}}))
+
 (deftest test-wait-times-by-day
   (testing "should calculate wait time for triggered build"
     (is (= {"deploy" {"1970-01-01" 800}}
-           (sut/wait-times-by-day [{:job "deploy"
-                                    :build-id "42"
-                                    :start 1000
-                                    :end 2000
-                                    :triggered-by {:job-name "test"
-                                                   :build-id "41"}}
-                                   {:job "test"
-                                    :build-id "41"
-                                    :end 200}]))))
+           (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")
+                                   (a-build "test" "41" 200)]))))
 
   (testing "should handle optional 'end' value for triggering build"
-    (is (empty? (sut/wait-times-by-day [{:job "deploy"
-                                         :build-id "42"
-                                         :start 1000
-                                         :end 2000
-                                         :triggered-by {:job-name "test"
-                                                        :build-id "41"}}
-                                        {:job "test"
-                                         :build-id "41"}]))))
+    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")
+                                        (-> (a-build "test" "41" 200)
+                                            (dissoc :end))]))))
 
   (testing "should handle optional 'end' value for triggered build"
-    (is (empty? (sut/wait-times-by-day [{:job "deploy"
-                                         :build-id "42"
-                                         :start 1000
-                                         :triggered-by {:job-name "test"
-                                                        :build-id "41"}}
-                                        {:job "test"
-                                         :build-id "41"
-                                         :end 200}]))))
+    (is (empty? (sut/wait-times-by-day [(-> (a-triggered-build "deploy" 1000 "test" "41")
+                                            (dissoc :end))
+                                        (a-build "test" "41" 200)]))))
 
   (testing "should handle missing triggering build"
-    (is (empty? (sut/wait-times-by-day [{:job "deploy"
-                                         :build-id "42"
-                                         :start 1000
-                                         :end 2000
-                                         :triggered-by {:job-name "test"
-                                                        :build-id "41"}}]))))
+    (is (empty? (sut/wait-times-by-day [(a-triggered-build "deploy" 1000 "test" "41")]))))
 
   (testing "should average wait times"
     (is (= {"deploy" {"1970-01-01" 900}}
-           (sut/wait-times-by-day [{:job "deploy"
-                                    :build-id "42"
-                                    :start 2000
-                                    :end 3000
-                                    :triggered-by {:job-name "test"
-                                                   :build-id "41"}}
-                                   {:job "test"
-                                    :build-id "41"
-                                    :end 1000}
-                                   {:job "deploy"
-                                    :build-id "30"
-                                    :start 1000
-                                    :end 2000
-                                    :triggered-by {:job-name "test"
-                                                   :build-id "30"}}
-                                   {:job "test"
-                                    :build-id "30"
-                                    :end 200}]))))
+           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" 2000 "test" "41")
+                                   (a-build "test" "41" 1000)
+                                   (a-triggered-build "deploy" "30" 1000 "test" "30")
+                                   (a-build "test" "30" 200)]))))
 
   (testing "should list wait times by day"
     (is (= {"deploy" {"1970-01-01" 800
                       "1970-01-02" 1000}}
-           (sut/wait-times-by-day [{:job "deploy"
-                                    :build-id "42"
-                                    :start (+ 2000 a-day)
-                                    :end (+ 3000 a-day)
-                                    :triggered-by {:job-name "test"
-                                                   :build-id "41"}}
-                                   {:job "test"
-                                    :build-id "41"
-                                    :end (+ 1000 a-day)}
-                                   {:job "deploy"
-                                    :build-id "30"
-                                    :start 1000
-                                    :end 2000
-                                    :triggered-by {:job-name "test"
-                                                   :build-id "30"}}
-                                   {:job "test"
-                                    :build-id "30"
-                                    :end 200}])))))
+           (sut/wait-times-by-day [(a-triggered-build "deploy" "42" (+ 2000 a-day) "test" "41")
+                                   (a-build "test" "41" (+ 1000 a-day))
+                                   (a-triggered-build "deploy" "30" 1000 "test" "30")
+                                   (a-build "test" "30" 200)])))))
