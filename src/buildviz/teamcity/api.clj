a103ce2e3422e4bb0dc3bdbaad81c58ad3eeb55d
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-04-09 22:45:26 +0800

    Support paging for big testsuites on TeamCity

--- src/buildviz/teamcity/api.clj
+++ src/buildviz/teamcity/api.clj
@@ -23,7 +23,18 @@
                            (format "/httpAuth/app/rest/buildTypes/id:%s/builds/?fields=build(id,number,status,startDate,finishDate)" job-id))]
     (get response :build)))
 
-(defn get-test-report [teamcity-url build-id]
+(def ^:private test-occurrence-paging-count 100)
+
+(defn get-test-report-from [teamcity-url build-id offset]
   (let [response (get-json teamcity-url
-                           (format "/httpAuth/app/rest/testOccurrences?locator=build:(id:%s)" build-id))]
-    (get response :testOccurrence)))
+                           (format "/httpAuth/app/rest/testOccurrences?locator=count:%s,start:%s,build:(id:%s)" test-occurrence-paging-count offset build-id))
+        test-occurrences (get response :testOccurrence)]
+    (if (< (count test-occurrences) test-occurrence-paging-count)
+      test-occurrences
+      (let [next-offset (+ offset test-occurrence-paging-count)]
+        (concat test-occurrences
+                (lazy-seq (get-test-report-from teamcity-url build-id next-offset)))))))
+
+
+(defn get-test-report [teamcity-url build-id]
+  (get-test-report-from teamcity-url build-id 0))
