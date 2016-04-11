bec4f7b68592e0159c74d4ab9f219eb90423d22a
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-04-11 23:37:55 +0800

    Refactor test

--- test/buildviz/teamcity/sync_test.clj
+++ test/buildviz/teamcity/sync_test.clj
@@ -9,28 +9,47 @@
   (fn [_] {:status 200
            :body (j/generate-string body)}))
 
+(defn- a-job [id project-name job-name]
+  {:id id
+   :projectName project-name
+   :name job-name})
+
+(defn- a-project [id & jobs]
+  [["http://teamcity:8000/httpAuth/app/rest/projects/the_project"
+    (successful-json-response {:buildTypes {:buildType jobs}})]])
+
+(defn- a-build [job-id build]
+  [[(format "http://teamcity:8000/httpAuth/app/rest/buildTypes/id:%s/builds/?fields=build(id,number,status,startDate,finishDate,revisions(revision(version,vcs-root-instance)))"
+            job-id)
+    (successful-json-response {:build [(merge {:revisions []}
+                                              build)]})]
+   [(format "http://teamcity:8000/httpAuth/app/rest/testOccurrences?locator=count:100,start:0,build:(id:%s)"
+            (:id build))
+    (successful-json-response {:testOccurrences []})]])
+
+(defn- capture-puts-to-buildviz-in [map-ref]
+  [[#"http://buildviz:8010/builds/(.+)/(.+)"
+    (fn [req]
+      (swap! map-ref #(assoc % (:uri req) (j/parse-string (slurp (:body req)) true)))
+      {:status 200 :body ""})]])
+
+(defn- serve-up [& routes]
+  (->> routes
+       (mapcat identity) ; flatten once
+       (into {})))
+
 (deftest test-main
   (testing "should sync a build"
-    (let [build-puts (atom {})]
-      (fake/with-fake-routes {"http://teamcity:8000/httpAuth/app/rest/projects/the_project"
-                              (successful-json-response {:buildTypes {:buildType [{:id "theId"
-                                                                                   :projectName "theProject"
-                                                                                   :name "theJob"}]}})
-                              "http://teamcity:8000/httpAuth/app/rest/buildTypes/id:theId/builds/?fields=build(id,number,status,startDate,finishDate,revisions(revision(version,vcs-root-instance)))"
-                              (successful-json-response {:build [{:id 42
-                                                                  :number 2
-                                                                  :status "SUCCESS"
-                                                                  :startDate "20160410T041049+0000"
-                                                                  :finishDate "20160410T041100+0000"
-                                                                  :revisions []}]})
-                              "http://teamcity:8000/httpAuth/app/rest/testOccurrences?locator=count:100,start:0,build:(id:42)"
-                              (successful-json-response {:testOccurrences []})
-                              #"http://buildviz:8010/builds/(.+)/(.+)"
-                              (fn [req]
-                                (swap! build-puts #(assoc % (:uri req) (j/parse-string (slurp (:body req)) true)))
-                                {:status 200 :body ""})}
+    (let [stored (atom {})]
+      (fake/with-fake-routes (serve-up (a-project "the_project" (a-job "theId" "theProject" "theJob"))
+                                       (a-build "theId" {:id 42
+                                                         :number 2
+                                                         :status "SUCCESS"
+                                                         :startDate "20160410T041049+0000"
+                                                         :finishDate "20160410T041100+0000"})
+                                       (capture-puts-to-buildviz-in stored))
         (with-out-str (sut/sync-jobs (url/url "http://teamcity:8000") (url/url "http://buildviz:8010") ["the_project"]))
         (is (= {:start 1460261449000
                 :end 1460261460000
                 :outcome "pass"}
-               (get @build-puts "/builds/theProject%20theJob/2")))))))
+               (get @stored "/builds/theProject%20theJob/2")))))))
