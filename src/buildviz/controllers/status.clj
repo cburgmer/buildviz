21d1c2b5cb19c50f350c22ca49e30649687f4201
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-04-18 21:37:39 +0800

    Refactor, only provide pipeline name if set

--- src/buildviz/controllers/status.clj
+++ src/buildviz/controllers/status.clj
@@ -2,16 +2,14 @@
   (:require [buildviz.data.results :as results]
             [buildviz.util.http :as http]))
 
-(defn- with-latest-build-start [all-builds response]
-  (if-let [build-starts (seq (remove nil? (map :start all-builds)))]
-    (assoc response
-           :latestBuildStart (apply max build-starts)
-           :earliestBuildStart (apply min build-starts))
-    response))
-
 (defn get-status [build-results pipeline-name]
   (let [all-builds (results/all-builds build-results)
-        total-build-count (count all-builds)]
-    (http/respond-with-json (with-latest-build-start all-builds
-                              {:totalBuildCount total-build-count
-                               :pipelineName pipeline-name}))))
+        total-build-count (count all-builds)
+        build-starts (->> all-builds
+                          (map :start)
+                          (remove nil?)
+                          seq)]
+    (http/respond-with-json (cond-> {:totalBuildCount total-build-count}
+                              pipeline-name (assoc :pipelineName pipeline-name)
+                              build-starts (assoc :latestBuildStart (apply max build-starts)
+                                                  :earliestBuildStart (apply min build-starts))))))
