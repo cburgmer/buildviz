83bc57824cc9f66f6341e5d42b35a3e962dcdccc
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-11-16 22:22:15 +0800

    Include green phases with fail phases

--- src/buildviz/analyse/fail_phases.clj
+++ src/buildviz/analyse/fail_phases.clj
@@ -1,50 +1,53 @@
 (ns buildviz.analyse.fail-phases
   (:require [buildviz.data.build-schema :as schema]))
 
-(defn- update-ongoing-fail-phase [fail-phases ongoing-fail-phase]
-  (conj (pop fail-phases) ongoing-fail-phase))
+(defn- new-phase [build]
+  (cond-> {:start (:end build)
+           :status (if (schema/failed-build? build) "fail" "pass")}
+    (schema/failed-build? build) (assoc :culprits #{(:job build)}
+                                        :ongoing-culprits #{(:job build)})))
 
-(defn- new-ongoing-phase [start culprit]
-  {:start start
-   :culprits #{culprit}
-   :ongoing-culprits #{culprit}})
+(defn- update-current-phase [phases current-phase]
+  (conj (pop phases) current-phase))
 
-(defn- open-new-fail-phase [fail-phases {job :job end :end}]
-  (conj fail-phases (new-ongoing-phase end job)))
+(defn- extend-phase [phases current-phase build]
+  (update-current-phase phases (assoc current-phase :end (:end build))))
 
-(defn- add-failing-build-to-fail-phase [fail-phases ongoing-fail-phase {job :job}]
-  (let [culprits (conj (:culprits ongoing-fail-phase) job)
-        ongoing-culprits (conj (:ongoing-culprits ongoing-fail-phase) job)]
-    (update-ongoing-fail-phase fail-phases
-                               (assoc ongoing-fail-phase
-                                      :culprits culprits
-                                      :ongoing-culprits ongoing-culprits))))
+(defn- close-phase-and-open-new [phases current-phase build]
+  (conj (update-current-phase phases (assoc current-phase :end (:end build)))
+        (new-phase build)))
 
-(defn- pop-passing-build-from-fail-phase [fail-phases ongoing-fail-phase {job :job end :end}]
-  (let [ongoing-culprits (disj (:ongoing-culprits ongoing-fail-phase) job)]
-    (update-ongoing-fail-phase fail-phases
-                               (assoc ongoing-fail-phase
-                                      :ongoing-culprits ongoing-culprits
-                                      :end end))))
+(defn- failing-phase-resolved? [phase]
+  (empty? (:ongoing-culprits phase)))
 
-(defn- accumulate-fail-phases [fail-phases build]
-  (let [job (:job build)]
-    (if (or (empty? fail-phases)
-            (empty? (:ongoing-culprits (last fail-phases))))
+(defn- accumulate-phases [phases build]
+  (if (empty? phases)
+    [(new-phase build)]
 
-      (if (schema/failed-build? build)
-        (open-new-fail-phase fail-phases build)
-        fail-phases)
+    (let [current-phase (last phases)]
+      (if (= "pass" (:status current-phase))
+        (if (schema/passed-build? build)
+          (extend-phase phases current-phase build)
+          (close-phase-and-open-new phases current-phase build))
 
-      (let [ongoing-fail-phase (last fail-phases)]
-        (if (and (schema/passed-build? build)
-                 (contains? (:ongoing-culprits ongoing-fail-phase) job))
-          (pop-passing-build-from-fail-phase fail-phases ongoing-fail-phase build)
+        (if (schema/failed-build? build)
+          (extend-phase phases
+                        (-> current-phase
+                            (update-in [:ongoing-culprits] conj (:job build))
+                            (update-in [:culprits] conj (:job build)))
+                        build)
+          (let [updated-phase (update-in current-phase [:ongoing-culprits] disj (:job build))]
+              (if (failing-phase-resolved? updated-phase)
+                (close-phase-and-open-new phases updated-phase build)
+                (extend-phase phases updated-phase build))))))))
 
-          (if (schema/failed-build? build)
-            (add-failing-build-to-fail-phase fail-phases ongoing-fail-phase build)
-            fail-phases))))))
+(defn pipeline-phases [builds]
+  (remove #(nil? (:end %))
+          (reduce accumulate-phases [] builds)))
 
+;; legacy
 (defn pipeline-fail-phases [builds]
-  (map #(dissoc % :ongoing-culprits)
-       (reduce accumulate-fail-phases [] builds)))
+  (->> builds
+       pipeline-phases
+       (filter #(= "fail" (:status %)))
+       (map #(dissoc % :ongoing-culprits :status))))
--- test/buildviz/analyse/fail_phases_test.clj
+++ test/buildviz/analyse/fail_phases_test.clj
@@ -3,52 +3,74 @@
             [clojure.test :refer :all]))
 
 (deftest PipelineInfo
+  (testing "pipeline-phases"
+    (is (= []
+           (pipeline-phases [])))
+    (is (= []
+           (pipeline-phases [{:end 20 :outcome "pass" :job "a job"}])))
+    (is (= []
+           (pipeline-phases [{:end 20 :outcome "fail" :job "a job"}])))
+    (is (= [{:start 20 :end 40 :status "pass"}]
+           (pipeline-phases [{:end 20 :outcome "pass" :job "a job"}
+                             {:end 40 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 20 :end 40 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 20 :outcome "fail" :job "a job"}
+                             {:end 40 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 20 :end 40 :status "fail" :culprits #{"a job"} :ongoing-culprits #{"a job"}}]
+           (pipeline-phases [{:end 20 :outcome "fail" :job "a job"}
+                             {:end 40 :outcome "fail" :job "a job"}])))
+    (is (= [{:start 20 :end 40 :status "pass"}
+            {:start 40 :end 60 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 20 :outcome "pass" :job "a job"}
+                             {:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 20 :end 40 :status "pass"}
+            {:start 40 :end 60 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}
+            {:start 60 :end 80 :status "pass"}
+            {:start 80 :end 100 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 20 :outcome "pass" :job "a job"}
+                             {:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "pass" :job "a job"}
+                             {:end 80 :outcome "fail" :job "a job"}
+                             {:end 100 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 40 :end 80 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "fail" :job "a job"}
+                             {:end 80 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 40 :end 80 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "pass" :job "another job"}
+                             {:end 80 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 40 :end 100 :status "fail" :culprits #{"a job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "pass" :job "another job"}
+                             {:end 80 :outcome "fail" :job "a job"}
+                             {:end 100 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 40 :end 100 :status "fail" :culprits #{"a job", "another job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "fail" :job "another job"}
+                             {:end 80 :outcome "pass" :job "a job"}
+                             {:end 100 :outcome "pass" :job "another job"}])))
+    (is (= [{:start 40 :end 100 :status "fail" :culprits #{"a job", "another job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "fail" :job "another job"}
+                             {:end 80 :outcome "pass" :job "another job"}
+                             {:end 100 :outcome "pass" :job "a job"}])))
+    (is (= [{:start 40 :end 80 :status "fail" :culprits #{"a job", "another job"} :ongoing-culprits #{"a job"}}]
+           (pipeline-phases [{:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "fail" :job "another job"}
+                             {:end 80 :outcome "pass" :job "another job"}])))
+    (is (= [{:start 20 :end 40 :status "pass"}
+            {:start 40 :end 140 :status "fail" :culprits #{"a job", "another job", "yet another job"} :ongoing-culprits #{}}]
+           (pipeline-phases [{:end 20 :outcome "pass" :job "a job"}
+                             {:end 40 :outcome "fail" :job "a job"}
+                             {:end 60 :outcome "fail" :job "another job"}
+                             {:end 80 :outcome "pass" :job "another job"}
+                             {:end 100 :outcome "fail" :job "yet another job"}
+                             {:end 120 :outcome "pass" :job "yet another job"}
+                             {:end 140 :outcome "pass" :job "a job"}]))))
+
   (testing "pipeline-fail-phases"
-    (is (= []
-           (pipeline-fail-phases [])))
-    (is (= []
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 20 :end 40 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "fail" :job "a job"}
-                                  {:end 40 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 60 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 60 :culprits #{"a job"}} {:start 80 :end 100 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "pass" :job "a job"}
-                                  {:end 80 :outcome "fail" :job "a job"}
-                                  {:end 100 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 80 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "fail" :job "a job"}
-                                  {:end 80 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 80 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "pass" :job "another job"}
-                                  {:end 80 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 100 :culprits #{"a job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "pass" :job "another job"}
-                                  {:end 80 :outcome "fail" :job "a job"}
-                                  {:end 100 :outcome "pass" :job "a job"}])))
-    (is (= [{:start 40 :end 100 :culprits #{"a job", "another job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "fail" :job "another job"}
-                                  {:end 80 :outcome "pass" :job "a job"}
-                                  {:end 100 :outcome "pass" :job "another job"}])))
-    (is (= [{:start 40 :end 100 :culprits #{"a job", "another job"}}]
-           (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
-                                  {:end 40 :outcome "fail" :job "a job"}
-                                  {:end 60 :outcome "fail" :job "another job"}
-                                  {:end 80 :outcome "pass" :job "another job"}
-                                  {:end 100 :outcome "pass" :job "a job"}])))
     (is (= [{:start 40 :end 140 :culprits #{"a job", "another job", "yet another job"}}]
            (pipeline-fail-phases [{:end 20 :outcome "pass" :job "a job"}
                                   {:end 40 :outcome "fail" :job "a job"}
