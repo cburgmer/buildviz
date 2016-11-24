0bd41347a2ea4c1847403ef5574ab33dc605f69e
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-11-24 22:41:32 +0800

    Stop trying to guess job groups from names for special color coding. Only really worked well for Go setup. Wait for proper implementation via #31

--- resources/public/common/jobColors.js
+++ resources/public/common/jobColors.js
@@ -2,47 +2,13 @@ var jobColors = function () {
     "use strict";
     var module = {};
 
-    var jobGroupColor,
-        jobGroupRange = {},
-        sameJobGroupColorSteps = 20,
-        subGroupBrightnessRange = d3.range(-sameJobGroupColorSteps / 2, sameJobGroupColorSteps / 2)
-            .sort(function (a, b) { return Math.abs(a) - Math.abs(b); });
-
-    var getJobGroupRange = function (jobGroup) {
-        if (! jobGroupRange[jobGroup]) {
-            jobGroupRange[jobGroup] = d3.scale
-                .ordinal()
-                .range(subGroupBrightnessRange);
-        }
-
-        return jobGroupRange[jobGroup];
-    };
-
-    var getJobGroup = function (jobName) {
-        return jobName.split(' ')[0];
-    };
-
-    var jobColor = function (jobName) {
-
-        var jobGroup = getJobGroup(jobName),
-            color = jobGroupColor(jobGroup),
-            jobRange = getJobGroupRange(jobGroup);
-
-        return d3.hsl(color)
-            .brighter(jobRange(jobName) / 8)
-            .toString();
-    };
-
-    var getStableJobGroups = function (jobNames) {
-        var jobGroups = d3.set(jobNames.map(getJobGroup)).values();
-        jobGroups.sort();
-        return jobGroups;
-    };
+    var jobColor;
 
     module.colors = function (jobNames) {
-        if (! jobGroupColor) {
-            jobGroupColor = d3.scale.category20()
-                .domain(getStableJobGroups(jobNames));
+        if (! jobColor) {
+            jobNames.sort();
+            jobColor = d3.scale.category20()
+                .domain(jobNames);
         }
 
         return jobColor;
