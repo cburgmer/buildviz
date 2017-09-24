465cd1afdd763e43b5618e0086c0465633aec038
Christoph Burgmer <christoph.burgmer@gmail.com>
2017-09-24 17:59:06 +0200

    Improve Teamcity example setup documentation

--- examples/runTeamCityExample.sh
+++ examples/runTeamCityExample.sh
@@ -5,8 +5,8 @@ TeamCity has a manual setup process, amongst that a license agreement you need t
 
 Here's what you can do:
 
-1. $ vagrant up
-2. Now click through and accept the license (avoid altering any settings on the way), choose admin:admin as credentials.
+1. $ cd examples/teamcity && vagrant up
+2. Now go to http://localhost:8111 and click through, accepting the license (avoid altering any settings on the way), choose admin:admin as credentials.
 3. Then switch to http://localhost:8111/project.html?projectId=SimpleSetup&tab=projectOverview and start some builds.
 4. In one tab: $ ./lein do deps, ring server-headless
 5. In another: $ ./lein run -m buildviz.teamcity.sync http://admin:admin@localhost:8111 -p SimpleSetup
