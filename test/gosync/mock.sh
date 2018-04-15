829183280e89abe26afc1d8b40e433e12280c0e3
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-04-15 11:00:03 +0200

    Upgrade wiremock dependency

--- test/gosync/mock.sh
+++ test/gosync/mock.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-VERSION="1.57"
-FILENAME="wiremock-${VERSION}-standalone.jar"
+VERSION="2.16.0"
+ARTIFACT_NAME="wiremock-standalone"
+FILENAME="${ARTIFACT_NAME}-${VERSION}.jar"
 
 if [ ! -f "${FILENAME}" ]; then
-    curl -O "http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock/${VERSION}/${FILENAME}"
+    curl -O "http://repo1.maven.org/maven2/com/github/tomakehurst/${ARTIFACT_NAME}/${VERSION}/${FILENAME}"
 fi
 
 java -jar "${FILENAME}" --port 3334
