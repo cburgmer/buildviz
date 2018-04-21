efce9b7ba4593a7ae0908a50001b743111781150
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-04-22 00:33:52 +0200

    Fix Jenkins example to work with latest release (2.117)

--- examples/jenkins/provision.sh
+++ examples/jenkins/provision.sh
@@ -3,25 +3,22 @@
 set -eo pipefail
 
 jenkins_queue_length() {
-    curl -H "$CRUMB" http://localhost:8080/queue/api/json | python -c "import json; import sys; print len(json.loads(sys.stdin.read())['items'])"
+    curl --silent http://localhost:8080/queue/api/json | python -c "import json; import sys; print len(json.loads(sys.stdin.read())['items'])"
 }
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get -qy install default-jre
-sudo apt-get -qy install curl git
-
-sudo apt-get -qy install jenkins xmlstarlet
+sudo apt-get -qy install default-jre curl git python
+sudo apt-get -qy install jenkins # only install after all dependencies are already available
 
 echo "Disabling Jenkins security"
 while [ ! -s /var/lib/jenkins/secrets/initialAdminPassword ]; do sleep 1; done
 sudo service jenkins stop
 
 # disable security, see https://jenkins.io/doc/book/operating/security/#disabling-security
-xmlstarlet edit --inplace -u '/hudson/useSecurity' -v 'false' /var/lib/jenkins/config.xml
-xmlstarlet edit --inplace -d '/hudson/authorizationStrategy' /var/lib/jenkins/config.xml
-xmlstarlet edit --inplace -d '/hudson/securityRealm' /var/lib/jenkins/config.xml
+sleep 10
+sudo rm /var/lib/jenkins/config.xml
 
 # disable showing the wizard on the first access.
 cp -p /var/lib/jenkins/jenkins.install.UpgradeWizard.state /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
@@ -29,12 +26,11 @@ cp -p /var/lib/jenkins/jenkins.install.UpgradeWizard.state /var/lib/jenkins/jenk
 sudo service jenkins start
 
 echo "Waiting for Jenkins to come up"
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do sleep 1; done
+until curl --output /dev/null --silent --head --fail http://localhost:8080; do sleep 1; done
 
 echo "Installing plugins"
 # Need to fetch plugin list first, Jenkins fails with 'No update center data is retrieved yet from: https://updates.jenkins.io/update-center.json' if we don't do this manually
-CRUMB=$(curl -s 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl --silent -X POST -H "$CRUMB" http://localhost:8080/pluginManager/checkUpdatesServer > /dev/null
+curl --silent -X POST http://localhost:8080/pluginManager/checkUpdatesServer > /dev/null
 
 for PLUGIN in git promoted-builds git-client parameterized-trigger build-pipeline-plugin dashboard-view; do
     sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin "${PLUGIN}"
@@ -43,9 +39,7 @@ done
 sudo service jenkins restart
 
 echo "Waiting for Jenkins to come up"
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do sleep 1; done
-
-CRUMB=$(curl -s 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+until curl --output /dev/null --silent --head --fail http://localhost:8080; do sleep 1; done
 
 # Create or Update
 echo "Configuring pipeline"
@@ -53,20 +47,20 @@ pushd .
 cd /mnt/jobs
 for JOB_CONFIG in *; do
     JOB_NAME=$( echo $JOB_CONFIG | sed s/.xml$// )
-    curl --silent -X POST -H "$CRUMB" --data-binary "@/mnt/jobs/$JOB_CONFIG" -H "Content-Type: application/xml" "http://localhost:8080/createItem?name=$JOB_NAME" > /dev/null
-    curl --silent -X POST -H "$CRUMB" --data-binary "@/mnt/jobs/$JOB_CONFIG" "http://localhost:8080/job/$JOB_NAME/config.xml" > /dev/null
+    curl --silent -X POST --data-binary "@/mnt/jobs/$JOB_CONFIG" -H "Content-Type: application/xml" "http://localhost:8080/createItem?name=$JOB_NAME" > /dev/null
+    curl --silent -X POST --data-binary "@/mnt/jobs/$JOB_CONFIG" "http://localhost:8080/job/$JOB_NAME/config.xml" > /dev/null
 done
 cd /mnt/views
 for VIEW_CONFIG in *; do
     VIEW_NAME=$( echo $VIEW_CONFIG | sed s/.xml$// )
-    curl --silent -X POST -H "$CRUMB" --data-binary "@/mnt/views/$VIEW_CONFIG" -H "Content-Type: application/xml" "http://localhost:8080/createView?name=$VIEW_NAME" > /dev/null
-    curl --silent -X POST -H "$CRUMB" --data-binary "@/mnt/views/$VIEW_CONFIG" "http://localhost:8080/view/$VIEW_NAME/config.xml" > /dev/null
+    curl --silent -X POST --data-binary "@/mnt/views/$VIEW_CONFIG" -H "Content-Type: application/xml" "http://localhost:8080/createView?name=$VIEW_NAME" > /dev/null
+    curl --silent -X POST --data-binary "@/mnt/views/$VIEW_CONFIG" "http://localhost:8080/view/$VIEW_NAME/config.xml" > /dev/null
 done
 popd
 
 echo "Triggering builds"
 for I in 1 2 3 4 5; do
-    curl --silent -X POST -H "$CRUMB" http://localhost:8080/job/Test/build > /dev/null
+    curl --silent -X POST http://localhost:8080/job/Test/build > /dev/null
 
     # Wait for build to run to enqueue next
     sleep 2
