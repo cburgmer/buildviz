322793bfcd55904669dd34eec5b0dc5816b0663e
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-04-23 23:11:16 +0200

    Simple run script for Teamcity

---
+++ examples/teamcity/run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -eo pipefail
+
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+readonly TMP_LOG="/tmp/run.$$.log"
+readonly BASE_URL="http://localhost:8111/mnt"
+
+wait_for_server() {
+    local url=$1
+    echo -n " waiting for ${url}"
+    until curl --output /dev/null --silent --head --fail "$url"; do
+        printf '.'
+        sleep 5
+    done
+}
+
+announce() {
+    local text="$1"
+    echo -ne "\033[1;30m"
+    echo -n "$text"
+    echo -ne "\033[0m"
+}
+
+hint_at_logs() {
+    # shellcheck disable=SC2181
+    if [[ "$?" -ne 0 ]]; then
+        echo
+        echo "Logs are in ${TMP_LOG}"
+    fi
+}
+
+goal_start() {
+    announce "Starting vagrant image"
+    echo -n " (give it a few minutes)"
+    (
+        cd "$SCRIPT_DIR"
+        vagrant up > "$TMP_LOG"
+    )
+
+    wait_for_server "$BASE_URL"
+    echo " done"
+    rm "$TMP_LOG"
+}
+
+goal_stop() {
+    announce "Stopping vagrant image"
+    (
+        cd "$SCRIPT_DIR"
+        vagrant halt > "$TMP_LOG"
+    )
+    echo " done"
+    rm "$TMP_LOG"
+}
+
+main() {
+    trap hint_at_logs EXIT
+
+    if type -t "goal_$1" &>/dev/null; then
+        "goal_$1"
+    else
+        echo "usage: $0 (start|stop)"
+    fi
+}
+
+main "$@"
