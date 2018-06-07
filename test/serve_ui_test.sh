9e5922c80c0977b5d394fee363e05fdb448e1be6
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-06-07 22:04:12 +0200

    Use node http server, a bit faster than Python's simple server. Also works without Python installed

--- test/serve_ui_test.sh
+++ test/serve_ui_test.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$SCRIPT_DIR"
 if [ ! -d "node_modules" ]; then
-    npm install csscritic
+    npm install csscritic http-server
 fi
 
 echo "Please go to http://localhost:8000/test/RegressionRunner.html"
 
 cd "$SCRIPT_DIR/.."
-python -m SimpleHTTPServer
+"$SCRIPT_DIR"/node_modules/.bin/http-server -p 8000 -a 127.0.0.1
