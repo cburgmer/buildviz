4f0ffb01f8c6cfa8e2a6d49da4d45383ef1b3dc7
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-06-12 21:51:38 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject buildviz "0.11.1"
+(defproject buildviz "0.12.0"
   :description "Transparency for your build pipeline's results and runtime."
   :url "https://github.com/cburgmer/buildviz"
   :license {:name "BSD 2-Clause"
