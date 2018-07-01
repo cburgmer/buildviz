cburgmer/buildviz

commit a53e98f690c15a81eea1ab7b4873534ba2b4203b

Capture all project shell scripts for linting

src/docker/run.sh
@@ -2,11 +2,11 @@
 set -e
 
 goal_server() {
-    java -jar ${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR}
+    java -jar "${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR}"
 }
 
 goal_do() {
-    java -cp ${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR} "$@"
+    java -cp "${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR}" "$@"
 }
 
 goal_schedule() {

go
@@ -4,7 +4,7 @@ set -e
 readonly SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 goal_lint() {
-    shellcheck examples/**/*.sh examples/*.sh test/integration/*.sh
+    find . -name "*.sh" -not -path "./test/node_modules/*" | xargs shellcheck
 }
 
 goal_test_unit() {

commit 8db89971dd610700abc7dec36f953b23fffdcb21

Fix quoting, broken in a53e98f6

src/docker/Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:alpine
 
-ENV BUILDVIZ_SERVER_DIR="/buildviz" \
-    BUILDVIZ_JAR="buildviz-*-standalone.jar"
+ENV BUILDVIZ_SERVER_DIR="/buildviz"
 
 RUN addgroup buildviz && \
     adduser -D -G buildviz -h ${BUILDVIZ_SERVER_DIR} -s /bin/sh buildviz
@@ -11,7 +10,7 @@ USER buildviz
 WORKDIR /buildviz
 
 ADD run.sh ${BUILDVIZ_SERVER_DIR}/
-ADD ${BUILDVIZ_JAR} ${BUILDVIZ_SERVER_DIR}/
+ADD buildviz-*-standalone.jar ${BUILDVIZ_SERVER_DIR}/
 
 ENTRYPOINT ["/buildviz/run.sh"]
 CMD ["server"]

src/docker/run.sh
@@ -2,11 +2,11 @@
 set -e
 
 goal_server() {
-    java -jar "${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR}"
+    java -jar "${BUILDVIZ_SERVER_DIR}"/buildviz-*-standalone.jar
 }
 
 goal_do() {
-    java -cp "${BUILDVIZ_SERVER_DIR}/${BUILDVIZ_JAR}" "$@"
+    java -cp "${BUILDVIZ_SERVER_DIR}"/buildviz-*-standalone.jar "$@"
 }
 
 goal_schedule() {