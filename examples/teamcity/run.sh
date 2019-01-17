d8a9b23407d1a39c3329316a72b0f129bf622100
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-01-17 23:38:51 +0100

    Automate most of the TeamCity setup, guide user from script

--- examples/teamcity/run.sh
+++ examples/teamcity/run.sh
@@ -4,12 +4,25 @@ set -eo pipefail
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 readonly TMP_LOG="/tmp/run.$$.log"
-readonly BASE_URL="http://localhost:8111/mnt"
+readonly PORT="8111"
+readonly BASE_URL="http://localhost:${PORT}"
+readonly USER="admin"
+readonly PASSWORD="admin"
+readonly BASE_API_URL="http://${USER}:${PASSWORD}@localhost:${PORT}/httpAuth/app/rest"
 
 wait_for_server() {
     local url=$1
     echo -n " waiting for ${url}"
     until curl --output /dev/null --silent --head --fail "$url"; do
+        printf '.'
+        sleep 5
+    done
+}
+
+wait_for_server_200() {
+    local url=$1
+    echo -n " waiting for ${url} to return 200"
+    until [[ $(curl  --output /dev/null --silent --head --write-out "%{http_code}" "$url") -eq 200 ]]; do
         printf '.'
         sleep 5
     done
@@ -53,9 +66,37 @@ start_server() {
     announce "Starting docker image"
     docker_compose up -d &> "$TMP_LOG"
 
-    wait_for_server "$BASE_URL"
+    wait_for_server "${BASE_URL}/mnt"
     echo " done"
     rm "$TMP_LOG"
+}
+
+authorize_worker() {
+    curl -X PUT -H "Content-Type: text/plain" --data true "${BASE_API_URL}/agents/id:1/authorized" &> "$TMP_LOG"
+    rm "$TMP_LOG"
+}
+
+run_build() {
+    local buildId="$1"
+    curl -X POST -H "Content-Type: application/xml"  --data "<build><buildType id=\"${buildId}\"/></build>" "${BASE_API_URL}/buildQueue"
+}
+
+run_builds() {
+    announce "Running builds"
+    run_build "SimpleSetup_Test" &> "$TMP_LOG"
+    run_build "SimpleSetup_RSpecJUnitXml" &> "$TMP_LOG"
+    run_build "SimpleSetup_RSpec" &> "$TMP_LOG"
+    run_build "SimpleSetup_SubProject_Test" &> "$TMP_LOG"
+    echo " done"
+    rm "$TMP_LOG"
+}
+
+provision_teamcity() {
+    announce "Please manually create the admin user via the web UI ${BASE_URL}, user '${USER}' with password '${PASSWORD}' (I'll wait)"
+    wait_for_server_200 "${BASE_API_URL}/server"
+    echo "Thank you"
+    authorize_worker
+    run_builds
 }
 
 goal_start() {
@@ -64,6 +105,7 @@ goal_start() {
         echo
         provision_container
         start_server
+        provision_teamcity
     else
         start_server
     fi
@@ -85,8 +127,8 @@ goal_destroy() {
 
 goal_purge() {
     announce "Purging docker images"
-    docker rmi jetbrains/teamcity-server:2018.1.2 >> "$TMP_LOG"
-    docker rmi jetbrains/teamcity-minimal-agent:2018.1.2 >> "$TMP_LOG"
+    docker rmi jetbrains/teamcity-server:2018.1.2 &> "$TMP_LOG"
+    docker rmi jetbrains/teamcity-minimal-agent:2018.1.2 &> "$TMP_LOG"
     echo " done"
     rm "$TMP_LOG"
 }
