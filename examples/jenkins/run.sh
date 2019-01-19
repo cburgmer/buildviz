05ad76aae0c28c5c97ca3d9b700cc47a3f05b021
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-01-19 14:31:28 +0100

    Bump Jenkins dependency with security fixes. Don't hide output for longrunning download process

--- examples/jenkins/run.sh
+++ examples/jenkins/run.sh
@@ -39,7 +39,7 @@ container_exists() {
 }
 
 provision_container() {
-    docker container create -p 8080:8080 --name buildviz_jenkins_example jenkins/jenkins:2.157-alpine &> "$TMP_LOG"
+    docker container create -p 8080:8080 --name buildviz_jenkins_example jenkins/jenkins:2.160-alpine
 }
 
 start_server() {
