76edde25b9a244731a5e0d148884c2dbe829ac4b
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-03-23 23:51:23 +0100

    Fix wording

--- examples/runSeedDataExample.sh
+++ examples/runSeedDataExample.sh
@@ -7,8 +7,8 @@ readonly BUILDVIZ_PORT=3333
 readonly BUILDVIZ_PATH="http://localhost:${BUILDVIZ_PORT}"
 
 ensure_port_available() {
-    if curl --output /dev/null --silent --head --fail "${BUILDVIZ_PATH}"; then
-        echo "Please stop the application running on port $PORT before continuing"
+    if curl --output /dev/null --silent --head --fail "$BUILDVIZ_PATH"; then
+        echo "Please stop the application running on port ${PORT} before continuing"
         exit 1
     fi
 }
@@ -23,7 +23,7 @@ main() {
     # Handle Ctrl+C
     trap clean_up EXIT
 
-    PORT="$BUILDVIZ_PORT" "${SCRIPT_DIR}/data/run_buildviz.sh" start
+    PORT="$BUILDVIZ_PORT" "$SCRIPT_DIR"/data/run_buildviz.sh start
 
     echo "Seeding data..."
     PORT="$BUILDVIZ_PORT" "$SCRIPT_DIR"/data/seedDummyData.sh
@@ -32,7 +32,7 @@ main() {
     echo
     echo "Point your browser to ${BUILDVIZ_PATH}"
     echo
-    echo "Later, press any key to stop the server and bring down the vagrant box"
+    echo "Later, press any key to stop the server"
 
     read -rn 1
 }
