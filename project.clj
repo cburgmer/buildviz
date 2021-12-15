263294ae4e40f4511e25b72f49ec43ee2fce83d5
Christoph Burgmer <christoph.burgmer@gmail.com>
2021-12-15 09:33:41 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "BSD 2-Clause"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/tools.logging "1.2.1"]
-                 [org.apache.logging.log4j/log4j-api "2.15.0"]
-                 [org.apache.logging.log4j/log4j-core "2.15.0"]
+                 [org.clojure/tools.logging "1.2.2"]
+                 [org.apache.logging.log4j/log4j-api "2.16.0"]
+                 [org.apache.logging.log4j/log4j-core "2.16.0"]
                  [ring/ring-core "1.9.4"]
                  [ring/ring-jetty-adapter "1.9.4"]
                  [ring/ring-json "0.5.1"]
