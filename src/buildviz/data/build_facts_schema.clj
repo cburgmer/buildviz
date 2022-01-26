479bafebbffcb1a23e8412db683ccda63f452df6
Christoph Burgmer <christoph.burgmer@gmail.com>
2022-01-26 22:25:57 +0100

    Update build-facts schema and use the one provided by the project directly

--- src/buildviz/data/build_facts_schema.clj
+++ src/buildviz/data/build_facts_schema.clj
@@ -3,40 +3,7 @@
   (:import clojure.lang.ExceptionInfo))
 
 (def build-schema
-  {:$schema "http://json-schema.org/draft-07/schema"
-   :type "object"
-   :required [:jobName :buildId :start]
-   :properties {:jobName {:type "string"}
-                :buildId {:type "string"}
-                :start {:type "integer" :minimum 0}
-                :end {:type "integer" :minimum 0}
-                :outcome {:type "string" :enum ["pass" "fail"]}
-                :inputs {:type "array"
-                         :items {:type "object"
-                                 :required [:sourceId :revision]
-                                 :properties {:sourceId {:type "string"}
-                                              :revision {:type "string"}}
-                                 :additionalProperties false}}
-                :triggeredBy {:type "array"
-                              :items {:type "object"
-                                      :required [:jobName :buildId]
-                                      :properties {:jobName {:type "string"}
-                                                   :buildId {:type "string"}}
-                                      :additionalProperties false}}
-                :testResults {:type "array"
-                              :items {:type "object"
-                                      :required [:name :children]
-                                      :properties {:name {:type "string"}
-                                                   :children {:type "array"
-                                                              :items {:type "object"
-                                                                      :required [:classname :name :status]
-                                                                      :properties {:classname {:type "string"}
-                                                                                   :name {:type "string"}
-                                                                                   :runtime {:type "integer" :minimum 0}
-                                                                                   :status {:type "string" :enum ["pass" "fail" "skipped" "error"]}}
-                                                                      :additionalProperties false}}}
-                                      :additionalProperties false}}}
-   :additionalProperties false})
+  (slurp (clojure.java.io/resource "schema.json")))
 
 (defn validation-errors [build]
   (try
